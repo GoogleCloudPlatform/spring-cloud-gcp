3c22eff1a9f4f005d2964180f3833f10ef2dcbd1
gkatzioura <gkatzioura@gmail.com>
2020-12-13 10:57:41 +0000

    Added JavaDoc

--- spring-cloud-gcp-kms/src/main/java/com/google/cloud/spring/kms/KMSOperations.java
+++ spring-cloud-gcp-kms/src/main/java/com/google/cloud/spring/kms/KMSOperations.java
@@ -18,12 +18,45 @@ package com.google.cloud.spring.kms;
 
 /**
  * Describes supported operations that one can perform on the KMS API.
+ *
+ * <p>For the methods you need specify the secret from GCP KMS by URI string.
+ * The following secret URI syntax is supported:
+ *
+ * 1. Shortest form - specify location ID, key ring ID, and key ID. Project is derived through {@link GcpProjectIdProvider}
+ * kms://{location-id}/{key-ring-id}/{key-id}
+ *
+ * 2.  Short form - specify project ID, location ID, key ring ID, and key ID
+ * kms://{project-id}/{location-id}/{key-ring-id}/{key-id}
+ *
+ * 3. Long form - specify project ID, location ID, key ring ID, and key ID
+ * kms://projects/{project-id}/locations/{location-id}/keyRings/{key-ring-id}/cryptoKeys/{key-id}
+ *
  * @author Emmanouil Gkatziouras
  */
 public interface KMSOperations {
 
+	/**
+	 * Encrypt the text using the specified KMS URI string {@code cryptoKey}.
+	 *
+	 * <p>
+	 * An encryption request will be issued using GCP KMS. The encrypted bytes received the response are Bas64 encoded.
+	 *
+	 * @param cryptoKey The KMS URI string
+	 * @param text The text to encrypt
+	 * @return The encrypted bytes in Base64 encoding
+	 */
 	String encrypt(String cryptoKey, String text);
 
+	/**
+	 * Decrypt the text using the specified KMS URI string {@code cryptoKey}.
+	 *
+	 * <p>
+	 * An decryption request will be issued using GCP KMS. The encrypted shall be Bas64 decode to extract the encrypted bytes for the request.
+	 *
+	 * @param cryptoKey The KMS URI string
+	 * @param encryptedText The encrypted bytes in Base64 encoding
+	 * @return The decrypted text
+	 */
 	String decrypt(String cryptoKey, String encryptedText);
 
 }
