fd3abbd87229552a11bfa0cdbc9dcdd46eb3f0af
Travis Tomsu <ttomsu@google.com>
2021-02-08 11:39:55 -0500

    Remove expressions that always evaluate to true/false

--- spring-cloud-gcp-data-datastore/src/main/java/com/google/cloud/spring/data/datastore/core/DatastoreTemplate.java
+++ spring-cloud-gcp-data-datastore/src/main/java/com/google/cloud/spring/data/datastore/core/DatastoreTemplate.java
@@ -411,7 +411,7 @@ public class DatastoreTemplate implements DatastoreOperations, ApplicationEventP
 		if (queryOptions.getCursor() != null) {
 			builder.setStartCursor(queryOptions.getCursor());
 		}
-		if (queryOptions.getSort() != null && persistentEntity != null) {
+		if (queryOptions.getSort() != null) {
 			queryOptions.getSort().stream()
 					.map((order) -> createOrderBy(persistentEntity, order))
 					.forEachOrdered((orderBy) -> builder.addOrderBy(orderBy));
