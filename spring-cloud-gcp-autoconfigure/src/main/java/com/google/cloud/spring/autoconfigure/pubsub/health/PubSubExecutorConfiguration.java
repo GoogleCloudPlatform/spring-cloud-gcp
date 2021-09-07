7c7980e2b0c909e8039bb4aa42bcb9ad09b550d6
gkatzioura <gkatzioura@gmail.com>
2021-09-08 00:15:48 +0100

    Added test for Health Indicator

---
+++ spring-cloud-gcp-autoconfigure/src/main/java/com/google/cloud/spring/autoconfigure/pubsub/health/PubSubExecutorConfiguration.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2017-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.spring.autoconfigure.pubsub.health;
+
+import com.google.api.gax.core.ExecutorProvider;
+import com.google.api.gax.core.FixedExecutorProvider;
+import com.google.cloud.spring.autoconfigure.pubsub.GcpPubSubProperties;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class PubSubExecutorConfiguration {
+
+	private final GcpPubSubProperties gcpPubSubProperties;
+
+	public PubSubExecutorConfiguration(
+		GcpPubSubProperties gcpPubSubProperties) {
+		this.gcpPubSubProperties = gcpPubSubProperties;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(name = "pubsubPublisherThreadPool")
+	public ThreadPoolTaskScheduler pubsubPublisherThreadPool() {
+		ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+		scheduler.setPoolSize(this.gcpPubSubProperties.getPublisher().getExecutorThreads());
+		scheduler.setThreadNamePrefix("gcp-pubsub-publisher");
+		scheduler.setDaemon(true);
+		return scheduler;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(name = "publisherExecutorProvider")
+	public ExecutorProvider publisherExecutorProvider(
+		@Qualifier("pubsubPublisherThreadPool") ThreadPoolTaskScheduler scheduler) {
+		return FixedExecutorProvider.create(scheduler.getScheduledExecutor());
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(name = "pubsubSubscriberThreadPool")
+	public ThreadPoolTaskScheduler pubsubSubscriberThreadPool() {
+		ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+		scheduler.setPoolSize(this.gcpPubSubProperties.getSubscriber().getExecutorThreads());
+		scheduler.setThreadNamePrefix("gcp-pubsub-subscriber");
+		scheduler.setDaemon(true);
+		return scheduler;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(name = "subscriberExecutorProvider")
+	public ExecutorProvider subscriberExecutorProvider(
+		@Qualifier("pubsubSubscriberThreadPool") ThreadPoolTaskScheduler scheduler) {
+		return FixedExecutorProvider.create(scheduler.getScheduledExecutor());
+	}
+
+
+}
---
+++ spring-cloud-gcp-autoconfigure/src/main/java/com/google/cloud/spring/autoconfigure/pubsub/health/PubSubSubscriptionHealthIndicatorAutoConfiguration.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2017-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.spring.autoconfigure.pubsub.health;
+
+import java.io.IOException;
+
+import com.google.api.gax.core.ExecutorProvider;
+import com.google.cloud.monitoring.v3.MetricServiceClient;
+import com.google.cloud.spring.autoconfigure.pubsub.GcpPubSubAutoConfiguration;
+import com.google.cloud.spring.autoconfigure.pubsub.GcpPubSubProperties;
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import com.google.cloud.spring.pubsub.core.health.HealthTrackerRegistry;
+import com.google.cloud.spring.pubsub.core.health.HealthTrackerRegistryImpl;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.health.CompositeHealthContributorConfiguration;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass({HealthIndicator.class})
+@ConditionalOnProperty({"spring.cloud.gcp.pubsub.subscription.lagThreshold", "spring.cloud.gcp.pubsub.subscription.backlogThreshold"})
+@AutoConfigureBefore(GcpPubSubAutoConfiguration.class)
+@EnableConfigurationProperties(GcpPubSubProperties.class)
+@Import(PubSubExecutorConfiguration.class)
+public class PubSubSubscriptionHealthIndicatorAutoConfiguration  extends
+	CompositeHealthContributorConfiguration<PubSubHealthIndicator, PubSubTemplate> {
+
+	@Bean
+	@ConditionalOnMissingBean
+	public MetricServiceClient metricServiceClient() throws IOException {
+		return MetricServiceClient.create();
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(name = "healthTrackerRegistry")
+	public HealthTrackerRegistry healthTrackerRegistry(
+		MetricServiceClient metricServiceClient,
+		@Value("${spring.cloud.gcp.pubsub.subscription.lagThreshold}") Integer lagThreshold,
+		@Value("${spring.cloud.gcp.pubsub.subscription.backlogThreshold}") Integer backlogThreshold,
+		@Qualifier("subscriberExecutorProvider") ExecutorProvider executorProvider) {
+		return new HealthTrackerRegistryImpl(metricServiceClient, lagThreshold, backlogThreshold, executorProvider);
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(name = {"pubSubSubscriptionHealthIndicator"})
+	public PubSubSubscriptionHealthIndicator pubSubSubscriptionHealthIndicator(HealthTrackerRegistry healthTrackerRegistry) {
+		return new PubSubSubscriptionHealthIndicator(healthTrackerRegistry);
+	}
+
+}
