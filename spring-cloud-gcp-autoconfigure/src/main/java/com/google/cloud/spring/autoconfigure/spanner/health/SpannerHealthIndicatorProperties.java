39f3d0192dffd8545b3605d84b576bda46b8fb65
ikeyat <kostinakate216@gmail.com>
2021-11-10 05:41:16 +0900

    Add SpannerHealthIndicator and AutoConfiguration (#643)
    Add SpannerHealthIndicator and AutoConfiguration.

---
+++ spring-cloud-gcp-autoconfigure/src/main/java/com/google/cloud/spring/autoconfigure/spanner/health/SpannerHealthIndicatorProperties.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.spring.autoconfigure.spanner.health;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties for Spanner Health Indicator.
+ *
+ * @author ikeyat
+ *
+ * @since 2.0.6
+ */
+@ConfigurationProperties("spring.cloud.gcp.spanner.health")
+public class SpannerHealthIndicatorProperties {
+
+	private String query = "SELECT 1";
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+}
