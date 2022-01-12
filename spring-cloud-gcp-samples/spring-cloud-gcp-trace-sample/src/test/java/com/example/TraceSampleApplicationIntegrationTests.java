5dd1ee8b0756afd954084d03df581f3b89da552e
ddixit14 <deepankardixit@google.com>
2022-01-12 11:22:23 -0500

    adding google style

--- spring-cloud-gcp-samples/spring-cloud-gcp-trace-sample/src/test/java/com/example/TraceSampleApplicationIntegrationTests.java
+++ spring-cloud-gcp-samples/spring-cloud-gcp-trace-sample/src/test/java/com/example/TraceSampleApplicationIntegrationTests.java
@@ -16,13 +16,8 @@
 
 package com.example;
 
-import java.io.IOException;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 import com.google.api.client.util.DateTime;
 import com.google.api.gax.core.CredentialsProvider;
@@ -50,6 +45,13 @@ import com.google.pubsub.v1.TopicName;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.auth.MoreCallCredentials;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -58,7 +60,6 @@ import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -69,212 +70,230 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
-
-/**
- * Verifies that the logged Traces on the sample application appear in StackDriver.
- */
-//Please use "-Dit.trace=true" to enable the tests
+/** Verifies that the logged Traces on the sample application appear in StackDriver. */
+// Please use "-Dit.trace=true" to enable the tests
 @EnabledIfSystemProperty(named = "it.trace", matches = "true")
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = { Application.class })
+@SpringBootTest(
+    webEnvironment = WebEnvironment.RANDOM_PORT,
+    classes = {Application.class})
 class TraceSampleApplicationIntegrationTests {
 
-	private final Logger log = LoggerFactory.getLogger(this.getClass());
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-	@LocalServerPort
-	private int port;
+  @LocalServerPort private int port;
 
-	@Autowired
-	private GcpProjectIdProvider projectIdProvider;
+  @Autowired private GcpProjectIdProvider projectIdProvider;
 
-	@Autowired
-	private CredentialsProvider credentialsProvider;
+  @Autowired private CredentialsProvider credentialsProvider;
 
-	private String url;
+  private String url;
 
-	private TestRestTemplate testRestTemplate;
+  private TestRestTemplate testRestTemplate;
 
-	private Logging logClient;
+  private Logging logClient;
 
-	private TraceServiceBlockingStub traceServiceStub;
+  private TraceServiceBlockingStub traceServiceStub;
 
-	private static final String SAMPLE_TOPIC = "traceTopic";
+  private static final String SAMPLE_TOPIC = "traceTopic";
 
-	private static final String SAMPLE_SUBSCRIPTION = "traceSubscription";
+  private static final String SAMPLE_SUBSCRIPTION = "traceSubscription";
 
-	final static String projectName = ProjectName.of(ServiceOptions.getDefaultProjectId()).getProject();
+  static final String projectName =
+      ProjectName.of(ServiceOptions.getDefaultProjectId()).getProject();
 
-	private static TopicAdminClient topicAdminClient;
+  private static TopicAdminClient topicAdminClient;
 
-	private static SubscriptionAdminClient subscriptionAdminClient;
+  private static SubscriptionAdminClient subscriptionAdminClient;
 
+  @BeforeAll
+  static void setup() throws IOException {
 
-	@BeforeAll
-	static void setup() throws IOException {
+    topicAdminClient = TopicAdminClient.create();
+    subscriptionAdminClient = SubscriptionAdminClient.create();
 
+    TopicName topic = TopicName.ofProjectTopicName(projectName, SAMPLE_TOPIC);
+    Topic response = topicAdminClient.createTopic(topic);
+    SubscriptionName subName = SubscriptionName.of(projectName, SAMPLE_SUBSCRIPTION);
+    PushConfig pushConfig = PushConfig.newBuilder().build();
+    int ackDeadlineSeconds = 599;
+    Subscription subResponse =
+        subscriptionAdminClient.createSubscription(subName, topic, pushConfig, ackDeadlineSeconds);
+  }
 
-		topicAdminClient = TopicAdminClient.create();
-		subscriptionAdminClient = SubscriptionAdminClient.create();
+  @AfterAll
+  static void cleanupPubsubClients() {
 
-		TopicName topic = TopicName.ofProjectTopicName(projectName, SAMPLE_TOPIC);
-		Topic response = topicAdminClient.createTopic(topic);
-		SubscriptionName subName = SubscriptionName.of(projectName, SAMPLE_SUBSCRIPTION);
-		PushConfig pushConfig = PushConfig.newBuilder().build();
-		int ackDeadlineSeconds = 599;
-		Subscription subResponse =
-				subscriptionAdminClient.createSubscription(subName, topic, pushConfig, ackDeadlineSeconds);
+    if (subscriptionAdminClient != null) {
+      deleteSubscriptions(SAMPLE_SUBSCRIPTION);
 
-	}
+      subscriptionAdminClient.close();
+    }
 
-	@AfterAll
-	static void cleanupPubsubClients() {
+    if (topicAdminClient != null) {
+      deleteTopics(SAMPLE_TOPIC);
+      topicAdminClient.close();
+    }
+  }
 
-		if (subscriptionAdminClient != null) {
-			deleteSubscriptions(SAMPLE_SUBSCRIPTION);
+  @BeforeEach
+  void setupTraceClient() throws IOException {
+    this.url = String.format("http://localhost:%d/", this.port);
 
-			subscriptionAdminClient.close();
-		}
+    // Create a new RestTemplate here because the auto-wired instance has built-in instrumentation
+    // which interferes with us setting the 'x-cloud-trace-context' header.
+    this.testRestTemplate = new TestRestTemplate();
 
-		if (topicAdminClient != null) {
-			deleteTopics(SAMPLE_TOPIC);
-			topicAdminClient.close();
-		}
+    this.logClient =
+        LoggingOptions.newBuilder()
+            .setProjectId(this.projectIdProvider.getProjectId())
+            .setCredentials(this.credentialsProvider.getCredentials())
+            .build()
+            .getService();
 
-	}
+    ManagedChannel channel =
+        ManagedChannelBuilder.forTarget("dns:///cloudtrace.googleapis.com").build();
 
-	@BeforeEach
-	void setupTraceClient() throws IOException {
-		this.url = String.format("http://localhost:%d/", this.port);
+    this.traceServiceStub =
+        TraceServiceGrpc.newBlockingStub(channel)
+            .withCallCredentials(
+                MoreCallCredentials.from(this.credentialsProvider.getCredentials()));
+  }
 
-		// Create a new RestTemplate here because the auto-wired instance has built-in instrumentation
-		// which interferes with us setting the 'x-cloud-trace-context' header.
-		this.testRestTemplate = new TestRestTemplate();
+  @Test
+  void testTracesAreLoggedCorrectly() {
+    DateTime startDateTime = new DateTime(System.currentTimeMillis() - 60000); // Time is hard.
 
-		this.logClient = LoggingOptions.newBuilder()
-				.setProjectId(this.projectIdProvider.getProjectId())
-				.setCredentials(this.credentialsProvider.getCredentials())
-				.build()
-				.getService();
+    HttpHeaders headers = new HttpHeaders();
 
-		ManagedChannel channel = ManagedChannelBuilder
-				.forTarget("dns:///cloudtrace.googleapis.com")
-				.build();
+    String uuidString = UUID.randomUUID().toString().replaceAll("-", "");
 
-		this.traceServiceStub = TraceServiceGrpc.newBlockingStub(channel)
-				.withCallCredentials(MoreCallCredentials.from(this.credentialsProvider.getCredentials()));
-	}
+    headers.add("x-cloud-trace-context", uuidString);
+    this.testRestTemplate.exchange(
+        this.url, HttpMethod.GET, new HttpEntity<>(headers), String.class);
 
-	@Test
-	void testTracesAreLoggedCorrectly() {
-		DateTime startDateTime = new DateTime(System.currentTimeMillis() - 60000); // Time is hard.
+    GetTraceRequest getTraceRequest =
+        GetTraceRequest.newBuilder()
+            .setProjectId(this.projectIdProvider.getProjectId())
+            .setTraceId(uuidString)
+            .build();
 
-		HttpHeaders headers = new HttpHeaders();
+    String logFilter =
+        String.format(
+            "trace=projects/%s/traces/%s AND logName=projects/%s/logs/spring.log AND"
+                + " timestamp>=\"%s\"",
+            this.projectIdProvider.getProjectId(),
+            uuidString,
+            this.projectIdProvider.getProjectId(),
+            startDateTime.toStringRfc3339());
 
-		String uuidString = UUID.randomUUID().toString().replaceAll("-", "");
+    await()
+        .atMost(4, TimeUnit.MINUTES)
+        .pollInterval(Duration.ofSeconds(2))
+        .ignoreExceptionsMatching(e -> e.getMessage().contains("Requested entity was not found"))
+        .untilAsserted(
+            () -> {
+              log.debug("Getting trace...");
+              Trace trace = this.traceServiceStub.getTrace(getTraceRequest);
+              log.info(
+                  "Found trace! "
+                      + trace.getTraceId()
+                      + " with "
+                      + trace.getSpansCount()
+                      + " spans ("
+                      + trace.getSpansList().stream()
+                          .map(TraceSpan::getName)
+                          .collect(Collectors.toList())
+                      + ").");
 
-		headers.add("x-cloud-trace-context", uuidString);
-		this.testRestTemplate.exchange(this.url, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+              assertThat(trace.getTraceId()).isEqualTo(uuidString);
+              // The 16 expected spans are:
+              // get /, visit-meet-endpoint, get, get /meet, get, get /meet, get, get /meet,
+              // send-message-spring-integration, publish, send-message-pub-sub-template, publish,
+              // next-message, on-message, next-message, on-message
+              assertThat(trace.getSpansCount()).isGreaterThanOrEqualTo(16);
+              log.debug("Trace spans match.");
 
-		GetTraceRequest getTraceRequest = GetTraceRequest.newBuilder()
-				.setProjectId(this.projectIdProvider.getProjectId())
-				.setTraceId(uuidString)
-				.build();
+              // verify custom tags
+              assertThat(trace.getSpans(0).getLabelsMap().get("environment")).isEqualTo("QA");
+              assertThat(trace.getSpans(0).getLabelsMap().get("session-id")).isNotNull();
+              log.debug("Trace labels match.");
 
-		String logFilter = String.format(
-				"trace=projects/%s/traces/%s AND logName=projects/%s/logs/spring.log AND timestamp>=\"%s\"",
-				this.projectIdProvider.getProjectId(), uuidString,
-				this.projectIdProvider.getProjectId(), startDateTime.toStringRfc3339());
+              assertThat(trace.getSpansList().stream().map(TraceSpan::getName).distinct())
+                  .containsExactly(
+                      "get /",
+                      "visit-meet-endpoint",
+                      "get",
+                      "get /meet",
+                      "send-message-spring-integration",
+                      "publish",
+                      "send-message-pub-sub-template",
+                      "next-message",
+                      "on-message");
+              log.debug("Trace span names match.");
 
-		await().atMost(4, TimeUnit.MINUTES)
-				.pollInterval(Duration.ofSeconds(2))
-				.ignoreExceptionsMatching(e -> e.getMessage().contains("Requested entity was not found"))
-				.untilAsserted(() -> {
+              List<LogEntry> logEntries = new ArrayList<>();
+              log.debug("Finding logs with filter: " + logFilter);
+              this.logClient
+                  .listLogEntries(Logging.EntryListOption.filter(logFilter))
+                  .iterateAll()
+                  .forEach(
+                      le -> {
+                        logEntries.add(le);
+                        log.debug("Found log entry: " + le.toString());
 
-			log.debug("Getting trace...");
-			Trace trace = this.traceServiceStub.getTrace(getTraceRequest);
-			log.info("Found trace! " + trace.getTraceId()
-					+ " with " + trace.getSpansCount() + " spans ("
-					+ trace.getSpansList().stream().map(TraceSpan::getName).collect(Collectors.toList())
-					+ ").");
+                        String wantTraceRegex =
+                            "projects/"
+                                + this.projectIdProvider.getProjectId()
+                                + "/traces/([a-z0-9]){32}";
+                        log.debug("Want trace " + wantTraceRegex + " and got " + le.getTrace());
+                        assertThat(le.getTrace()).matches(wantTraceRegex);
 
-			assertThat(trace.getTraceId()).isEqualTo(uuidString);
-			// The 16 expected spans are:
-			// get /, visit-meet-endpoint, get, get /meet, get, get /meet, get, get /meet,
-			// send-message-spring-integration, publish, send-message-pub-sub-template, publish,
-			// next-message, on-message, next-message, on-message
-			assertThat(trace.getSpansCount()).isGreaterThanOrEqualTo(16);
-			log.debug("Trace spans match.");
+                        String wantSpanRegex = "([a-z0-9]){16}";
+                        log.debug("Want span " + wantSpanRegex + " and got " + le.getSpanId());
+                        assertThat(le.getSpanId()).matches(wantSpanRegex);
 
-			// verify custom tags
-			assertThat(trace.getSpans(0).getLabelsMap().get("environment")).isEqualTo("QA");
-			assertThat(trace.getSpans(0).getLabelsMap().get("session-id")).isNotNull();
-			log.debug("Trace labels match.");
+                        log.debug("Log trace and span match.");
+                      });
 
-			assertThat(trace.getSpansList().stream().map(TraceSpan::getName).distinct())
-					.containsExactly(
-							"get /",
-							"visit-meet-endpoint",
-							"get",
-							"get /meet",
-							"send-message-spring-integration",
-							"publish",
-							"send-message-pub-sub-template",
-							"next-message",
-							"on-message");
-			log.debug("Trace span names match.");
+              List<String> logContents =
+                  logEntries.stream()
+                      .map(
+                          logEntry ->
+                              (String)
+                                  ((JsonPayload) logEntry.getPayload())
+                                      .getDataAsMap()
+                                      .get("message"))
+                      .collect(Collectors.toList());
 
-			List<LogEntry> logEntries = new ArrayList<>();
-			log.debug("Finding logs with filter: " + logFilter);
-			this.logClient.listLogEntries(Logging.EntryListOption.filter(logFilter)).iterateAll()
-					.forEach(le -> {
-						logEntries.add(le);
-						log.debug("Found log entry: " + le.toString());
+              log.info(
+                  "\n========================= [START OF LOG CONTENTS] =========================\n"
+                      + logContents.toString()
+                      + "\n"
+                      + "========================= [END OF LOG CONTENTS]  "
+                      + " =========================\n");
+              assertThat(logContents).contains("starting busy work");
+              log.debug("Found 'starting' line");
+              assertThat(logContents).contains("finished busy work");
+              log.debug("Found 'finishing' line");
+              assertThat(logContents)
+                  .contains("Message arrived! Payload: All work is done via PubSubTemplate.");
+              log.debug("Found 'PubSubTemplate' line");
+              assertThat(logContents)
+                  .contains("Message arrived! Payload: All work is done via SI.");
+              log.debug("Found 'SI' line");
+            });
+  }
 
-						String wantTraceRegex = "projects/" + this.projectIdProvider.getProjectId() + "/traces/([a-z0-9]){32}";
-						log.debug("Want trace " + wantTraceRegex + " and got " + le.getTrace());
-						assertThat(le.getTrace()).matches(wantTraceRegex);
+  private static void deleteSubscriptions(String testSubscription) {
 
-						String wantSpanRegex = "([a-z0-9]){16}";
-						log.debug("Want span " + wantSpanRegex + " and got " + le.getSpanId());
-						assertThat(le.getSpanId()).matches(wantSpanRegex);
+    String testSubscriptionName = ProjectSubscriptionName.format(projectName, testSubscription);
+    subscriptionAdminClient.deleteSubscription(testSubscriptionName);
+  }
 
-						log.debug("Log trace and span match.");
-					});
+  private static void deleteTopics(String testTopic) {
 
-
-			List<String> logContents = logEntries.stream()
-					.map(logEntry -> (String) ((JsonPayload) logEntry.getPayload())
-							.getDataAsMap().get("message"))
-					.collect(Collectors.toList());
-
-			log.info("\n========================= [START OF LOG CONTENTS] =========================\n"
-					+ logContents.toString()
-					+ "\n========================= [END OF LOG CONTENTS]   =========================\n");
-			assertThat(logContents).contains("starting busy work");
-			log.debug("Found 'starting' line");
-			assertThat(logContents).contains("finished busy work");
-			log.debug("Found 'finishing' line");
-			assertThat(logContents).contains("Message arrived! Payload: All work is done via PubSubTemplate.");
-			log.debug("Found 'PubSubTemplate' line");
-			assertThat(logContents).contains("Message arrived! Payload: All work is done via SI.");
-			log.debug("Found 'SI' line");
-		});
-	}
-
-	private static void deleteSubscriptions(String testSubscription) {
-
-		String testSubscriptionName = ProjectSubscriptionName.format(projectName, testSubscription);
-		subscriptionAdminClient.deleteSubscription(testSubscriptionName);
-
-		}
-
-
-	private static void deleteTopics(String testTopic) {
-
-		String testTopicName = ProjectTopicName.format(projectName, testTopic);
-		topicAdminClient.deleteTopic(testTopicName);
-
-		}
+    String testTopicName = ProjectTopicName.format(projectName, testTopic);
+    topicAdminClient.deleteTopic(testTopicName);
+  }
 }
