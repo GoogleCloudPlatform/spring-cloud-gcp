b2916c3ed318fff8a7945f28fb1fa22d09605b1c
Mridula <kostinakate216@gmail.com>
2022-01-24 11:30:45 -0500

    Introduce EnvironmentPostProcessor for R2DBC support in Cloud Sql  (#772)
    This PR adds two starters for R2DBC support in Cloud SQL (one for mySQL and another for postgreSQL). It also includes 2 samples, associated integrations tests and documentation. The starters bring in spring-boot-starter-data-r2dbc which takes care of the creation of the ConnectionFactory bean.

---
+++ spring-cloud-gcp-samples/spring-cloud-gcp-sql-mysql-r2dbc-sample/src/main/java/com/example/WebController.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Web app controller class for sample application. Contains a function that runs a query and
+ * displays the results.
+ */
+@RestController
+public class WebController {
+
+  private final ConnectionFactory connectionFactory;
+
+  public WebController(ConnectionFactory connectionFactory) {
+    this.connectionFactory = connectionFactory;
+  }
+
+  @GetMapping("/getTuples")
+  public Flux<String> getTuples() {
+    return Mono.from(connectionFactory.create())
+        .flatMapMany(connection -> connection.createStatement("SELECT * FROM users").execute())
+        .flatMap(
+            result ->
+                result.map(
+                    (row, metadata) ->
+                        String.format(
+                            "[%s, %s, %s]",
+                            row.get("EMAIL", String.class),
+                            row.get("FIRST_NAME", String.class),
+                            row.get("LAST_NAME", String.class))));
+  }
+}
