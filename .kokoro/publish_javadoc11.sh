16f78252275a8f7d699adc70012122f7b062273a
Min Zhu <zhumin@google.com>
2022-05-05 15:43:42 +0000

    more attempts to set java to 11.

--- .kokoro/publish_javadoc11.sh
+++ .kokoro/publish_javadoc11.sh
@@ -13,7 +13,8 @@ fi
 # switch to java 11
 sudo update-java-alternatives --set java-1.11.0-openjdk-amd64
 $JAVA_HOME/bin/javac -version
-
+export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+$JAVA_HOME/bin/javac -version
 
 pyenv global 3.7.2
 
