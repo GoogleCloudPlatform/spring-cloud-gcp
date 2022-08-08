e302533568c12256fe325bd79e078df0f9a12726
Joe Wang <joewa@google.com>
2022-08-08 14:55:51 -0400

    fix broken unit test

--- spring-cloud-gcp-autoconfigure/src/test/java/com/google/cloud/spring/autoconfigure/secretmanager/SecretManagerConfigDataLocationResolverUnitTests.java
+++ spring-cloud-gcp-autoconfigure/src/test/java/com/google/cloud/spring/autoconfigure/secretmanager/SecretManagerConfigDataLocationResolverUnitTests.java
@@ -4,12 +4,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.auth.Credentials;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.DefaultBootstrapContext;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.config.ConfigDataLocation;
 import org.springframework.boot.context.config.ConfigDataLocationResolverContext;
 import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Unit tests for {@link SecretManagerConfigDataLocationResolver}.
@@ -33,12 +40,28 @@ public class SecretManagerConfigDataLocationResolverUnitTests {
 
   @Test
   public void resolveReturnsConfigDataLocation() {
-    when(context.getBinder()).thenReturn(new Binder());
-    when(context.getBootstrapContext()).thenReturn(new DefaultBootstrapContext());
-    List<SecretManagerConfigDataResource> locations = resolver.resolve(context,
-        ConfigDataLocation.of("sm://my-secret"));
-    assertThat(locations.size()).isEqualTo(1);
-    assertThat(locations).first().extracting("location")
-        .isEqualTo(ConfigDataLocation.of("sm://my-secret"));
+    SpringApplicationBuilder applicationBuilder =
+        new SpringApplicationBuilder(
+            TestSecretManagerConfiguration.class)
+            .properties("spring.cloud.gcp.sql.enabled=false")
+            .web(WebApplicationType.NONE);
+    try (ConfigurableApplicationContext c = applicationBuilder.run()) {
+      when(context.getBinder()).thenReturn(new Binder());
+      when(context.getBootstrapContext()).thenReturn(new DefaultBootstrapContext());
+      List<SecretManagerConfigDataResource> locations = resolver.resolve(context,
+          ConfigDataLocation.of("sm://my-secret"));
+      assertThat(locations.size()).isEqualTo(1);
+      assertThat(locations).first().extracting("location")
+          .isEqualTo(ConfigDataLocation.of("sm://my-secret"));
+    }
+  }
+
+  @Configuration
+  static class TestSecretManagerConfiguration {
+
+    @Bean
+    public static CredentialsProvider googleCredentials() {
+      return () -> mock(Credentials.class);
+    }
   }
 }
