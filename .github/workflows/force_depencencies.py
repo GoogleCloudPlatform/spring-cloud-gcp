fa0c186181448d7d30762fa0c4e136b7f5e5f854
Tomo Suzuki <suztomo@google.com>
2022-10-26 15:13:47 -0400

    inserts but the entire file has strange ns0

--- .github/workflows/force_depencencies.py
+++ .github/workflows/force_depencencies.py
@@ -18,11 +18,29 @@ with open(pom_file_name) as pom_file:
   namespace_map = {
     'maven': maven_namespace
   }
-  xml.register_namespace('m', maven_namespace)
+#  xml.register_namespace('m', maven_namespace)
   parsed = xml.parse(pom_file)
   root = parsed.getroot()
-  dependencyManagement = root.find('maven:dependencyManagement', namespace_map)
-  print(dependencyManagement)
+  dependency_management = root.find('maven:dependencyManagement', namespace_map)
+  dependency_management_dependencies = dependency_management.find('maven:dependencies', namespace_map)
+  for forced_dependency in forced_dependencies:
+    forced_version = forced_dependencies[forced_dependency]
+    elements = forced_dependency.split(':')
+    group_id = elements[0]
+    artifact_id = elements[1]
+    dependency_element = xml.Element('dependency')
+    group_id_element = xml.Element('groupId')
+    dependency_element.append(group_id_element)
+    group_id_element.text = group_id
+    artifact_id_element = xml.Element('artifactId')
+    dependency_element.append(artifact_id_element)
+    artifact_id_element.text = artifact_id
+    version_element = xml.Element('version')
+    version_element.text = forced_version
+    dependency_element.append(version_element)
+    dependency_management_dependencies.insert(0, dependency_element)
+    pdb.set_trace()
   for child in root:
     print(child.tag, child.attrib)
-  pdb.set_trace()
+  parsed.write("pom_updated.xml")
+
