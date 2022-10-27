bbaf008e3c8d6067929b55194dabefc9816d3297
Tomo Suzuki <suztomo@google.com>
2022-10-27 12:08:51 -0400

    test the job

--- .github/workflows/force_depencencies.py
+++ .github/workflows/force_depencencies.py
@@ -5,13 +5,26 @@ import re
 
 import pdb
 
+repository = sys.argv[1]
 version_json_file = sys.argv[1]
 
 forced_dependencies = {}
 with open(version_json_file) as f:
   forced_dependencies = json.load(f)
 
+pom_file_name = "pom.xml"
+with open(pom_file_name, "r") as pom_file:
+  pom_file_content = pom_file.read()
+
+# Find the proper indent for the file. Using the same indent character as the
+# file makes the resulting pom.xml natural-looking.
+indent = ''
+indent_match = re.search(r'^(\s+)<', pom_file_content, re.MULTILINE)
+if indent_match:
+  indent = indent_match.group(1)
+
 section_to_add = ""
+base_indent_count = 3
 for forced_dependency in forced_dependencies:
   elements = forced_dependency.split(':')
   group_id = elements[0]
@@ -19,28 +32,25 @@ for forced_dependency in forced_dependencies:
   forced_version = forced_dependencies[forced_dependency]
   if artifact_id.endswith('-bom'):
     section_to_add += f'''
-          <dependency>
-            <groupId>{group_id}</groupId>
-            <artifactId>{artifact_id}</artifactId>
-            <version>{forced_version}</version>
-            <type>pom</type>
-            <scope>import</scope>
-          </dependency>
+    {indent*base_indent_count}<dependency>
+    {indent*(base_indent_count + 1)}<groupId>{group_id}</groupId>
+    {indent*(base_indent_count + 1)}<artifactId>{artifact_id}</artifactId>
+    {indent*(base_indent_count + 1)}<version>{forced_version}</version>
+    {indent*(base_indent_count + 1)}<type>pom</type>
+    {indent*(base_indent_count + 1)}<scope>import</scope>
+    {indent*base_indent_count}</dependency>
     '''
   else:
     section_to_add += f'''
-          <dependency>
-            <groupId>{group_id}</groupId>
-            <artifactId>{artifact_id}</artifactId>
-            <version>{forced_version}</version>
-          </dependency>
+    {indent*base_indent_count}<dependency>
+    {indent*(base_indent_count + 1)}<groupId>{group_id}</groupId>
+    {indent*(base_indent_count + 1)}<artifactId>{artifact_id}</artifactId>
+    {indent*(base_indent_count + 1)}<version>{forced_version}</version>
+    {indent*base_indent_count}</dependency>
     '''
 
-pom_file_name = "pom.xml"
-with open(pom_file_name, "r") as pom_file:
-  pom_file_content = pom_file.read()
-regex = re.compile(r"^.*x-cross-repo-check-dependency-mark.*$")
-pdb.set_trace()
+regex = re.compile(r"^.*x-cross-repo-check-dependency-mark.*$", re.MULTILINE)
+# pdb.set_trace()
 pom_file_content = regex.sub(section_to_add, pom_file_content)
 
 with open(pom_file_name, "w") as pom_file:
