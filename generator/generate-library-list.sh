7fefeb42da20ba946ed92305bce85c329fde8e2e
Min Zhu <zhumin@google.com>
2022-11-04 20:34:44 +0000

    rename output, add flag to use a specified commitish. minor comments for debug changes.

--- generator/generate-library-list.sh
+++ generator/generate-library-list.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+#cmd line:: ./generate-library-list.sh -c b2a5f7207e9f9b2f933e75ef4fd44973a234326f
+
+while getopts c: flag
+do
+    case "${flag}" in
+        c) commitish=${OPTARG};;
+    esac
+done
+echo "monorepo commitish to checkout: $commitish";
+
+
 # install jq for json parsing if not already installed
 sudo apt-get -y install jq
 
 # download the monorepo, need to loop through metadata there
 git clone https://github.com/googleapis/google-cloud-java.git
 
-# switch to latest release commitish
+# switch to the specified release commitish
 cd ./google-cloud-java
-git checkout b2a5f7207e9f9b2f933e75ef4fd44973a234326f
+git checkout $commitish
 cd -
 
-#cd ~/repos/google-cloud-java
-
-filename=./library_list_gen.txt
+# start file, always override is present
+filename=./library_list.txt
 echo "# api_shortname, distribution_name:version, library_type, transport, group_id, release_level" > $filename
+
 count=0
 for d in ./google-cloud-java/*java-*/; do
-  echo $d
   # parse variables from .repo-metadata.json
   language=$(cat $d/.repo-metadata.json | jq -r .language)
   api_shortname=$(cat $d/.repo-metadata.json | jq -r .api_shortname)
@@ -28,26 +38,26 @@ for d in ./google-cloud-java/*java-*/; do
 
   group_id=$(echo $distribution_name | cut -f1 -d:)
   if [[ $library_type != *GAPIC_AUTO* ]] ; then
-    echo "non auto type: $library_type"
+    echo "$d: non auto type: $library_type"
     continue
   fi
   if [[ $transport != *grpc* ]] ; then
-    echo "transport type not in scope: $transport"
+    echo "$d: transport type not in scope: $transport"
     continue
   fi
   if [[ $group_id != "com.google.cloud" ]] ; then
-    echo "group_id not in scope: $group_id"
+    echo "$d: group_id not in scope: $group_id"
     continue
   fi
   if [[ $release_level != "stable" ]] ; then
-    echo "release_level: $release_level"
+    echo "$d: release_level: $release_level"
     continue
   fi
 
   # get version from versions.txt
   module_versions=$(sed -n '4p' $d/versions.txt)
-  module=$(echo "$module_versions" | cut -f1 -d:)
-  released_version=$(echo "$module_versions" | cut -f2 -d:)
+#  module=$(echo "$module_versions" | cut -f1 -d:)
+#  released_version=$(echo "$module_versions" | cut -f2 -d:)
   current_version=$(echo "$module_versions" | cut -f3 -d:)
   echo "$api_shortname, $distribution_name:$current_version, $library_type, $transport, $group_id, $release_level" >> $filename
   count=$((count+1))
