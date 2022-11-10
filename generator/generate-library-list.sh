f6bdadad2d41008343a518243d1756fcae3eac4d
Min Zhu <zhumin@google.com>
2022-11-10 20:13:57 +0000

    trim un-needed fields from list. updated script to generate library_list.txt according to new changes in google-cloud-java.

--- generator/generate-library-list.sh
+++ generator/generate-library-list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#cmd line:: ./generate-library-list.sh -c b2a5f7207e9f9b2f933e75ef4fd44973a234326f
+#cmd line:: ./generate-library-list.sh -c 9ff8aa652e195965037706a3455a8e8aa318be5c
 
 while getopts c: flag
 do
@@ -19,12 +19,16 @@ git clone https://github.com/googleapis/google-cloud-java.git
 
 # switch to the specified release commitish
 cd ./google-cloud-java
-git checkout $commitish
+if [ -z "$commitish" ];
+  then echo "No commitish provided, using HEAD.";
+  else git checkout $commitish;
+fi
+
 cd -
 
 # start file, always override is present
 filename=./library_list.txt
-echo "# api_shortname, distribution_name:version, library_type, transport, group_id, release_level" > $filename
+echo "# api_shortname, googleapis-folder, distribution_name:version" > $filename
 
 count=0
 for d in ./google-cloud-java/*java-*/; do
@@ -37,6 +41,7 @@ for d in ./google-cloud-java/*java-*/; do
   release_level=$(cat $d/.repo-metadata.json | jq -r .release_level)
 
   group_id=$(echo $distribution_name | cut -f1 -d:)
+  artifact_id=$(echo $distribution_name | cut -f2 -d:)
   if [[ $library_type != *GAPIC_AUTO* ]] ; then
     echo "$d: non auto type: $library_type"
     continue
@@ -55,11 +60,18 @@ for d in ./google-cloud-java/*java-*/; do
   fi
 
   # get version from versions.txt
-  module_versions=$(sed -n '4p' $d/versions.txt)
+  # this changed in https://github.com/googleapis/google-cloud-java/pull/8755/
+  module_versions=$(sed -n "/^${artifact_id}/p" ./google-cloud-java/versions.txt)
+
 #  module=$(echo "$module_versions" | cut -f1 -d:)
-#  released_version=$(echo "$module_versions" | cut -f2 -d:)
-  current_version=$(echo "$module_versions" | cut -f3 -d:)
-  echo "$api_shortname, $distribution_name:$current_version, $library_type, $transport, $group_id, $release_level" >> $filename
+  released_version=$(echo "$module_versions" | cut -f2 -d:)
+#  current_version=$(echo "$module_versions" | cut -f3 -d:)
+
+  # infer googleapis folder from api_shortname:
+  # this is not reliable needs manual verification,
+  # will replace with metadata from google-cloud-java/artifact
+  googleapis_folder="//google/cloud/$api_shortname/v1"
+  echo "$api_shortname, $googleapis_folder, $distribution_name:$released_version" >> $filename
   count=$((count+1))
 done
 echo "Total in-scope client libraries: $count"
