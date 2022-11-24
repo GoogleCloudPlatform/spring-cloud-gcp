beba515505e060bf3b56e30437376364b0d4fffe
Diego Marquez <diegomarquezp@google.com>
2022-11-24 16:30:08 +0000

    fix: format and license header

--- generated/global-autoconfig-properties/src/main/java/com/google/cloud/spring/global/GlobalProperties.java
+++ generated/global-autoconfig-properties/src/main/java/com/google/cloud/spring/global/GlobalProperties.java
@@ -1,3 +1,17 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.cloud.spring.global;
 
 import com.google.cloud.spring.core.Credentials;
@@ -8,12 +22,11 @@ import org.springframework.boot.context.properties.NestedConfigurationProperty;
 @ConfigurationProperties("com.google.cloud.spring.global")
 public class GlobalProperties implements CredentialsSupplier {
 
-    // Overrides the GCP OAuth2 credentials specified in the Core module.
-    @NestedConfigurationProperty
-    private final Credentials credentials = new Credentials();
+  // Overrides the GCP OAuth2 credentials specified in the Core module.
+  @NestedConfigurationProperty private final Credentials credentials = new Credentials();
 
-    @Override
-    public Credentials getCredentials() {
-        return this.credentials;
-    }
+  @Override
+  public Credentials getCredentials() {
+    return this.credentials;
+  }
 }
