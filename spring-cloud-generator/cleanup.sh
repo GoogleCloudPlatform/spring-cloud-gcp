bf69a5001f8c66cda4f223cbeb5753b42ba98e9a
Diego Marquez <diegomarquezp@google.com>
2022-12-22 20:17:59 +0000

    chore: cleanup

--- spring-cloud-generator/cleanup.sh
+++ spring-cloud-generator/cleanup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# helper script that cleans the changes made by a generate-all.sh call
 git checkout HEAD -- ../spring-cloud-gcp-starters
 git checkout HEAD -- ../spring-cloud-previews
 rm -rdf ../spring-cloud-previews/google*
