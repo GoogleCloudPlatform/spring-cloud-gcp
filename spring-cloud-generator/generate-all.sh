ec3c4a3160de7c651933b61f841b1d27de9705da
Diego Marquez <diegomarquezp@google.com>
2023-01-04 22:26:25 +0000

    fix: remove unnecessary `$version` variable in `generate-*.sh`

--- spring-cloud-generator/generate-all.sh
+++ spring-cloud-generator/generate-all.sh
@@ -16,8 +16,7 @@ while IFS=, read -r library_name googleapis_location coordinates_version; do
   echo "processing library $library_name"
   group_id=$(echo $coordinates_version | cut -f1 -d:)
   artifact_id=$(echo $coordinates_version | cut -f2 -d:)
-  version=$(echo $coordinates_version | cut -f3 -d:)
-  bash $WORKING_DIR/generate-one.sh -c $library_name -v $version -i $artifact_id -g $group_id -p $PROJECT_VERSION -f $googleapis_location
+  bash $WORKING_DIR/generate-one.sh -c $library_name -i $artifact_id -g $group_id -p $PROJECT_VERSION -f $googleapis_location
 done <<< $libraries
 
 
