307caa3cde7a29c0c70e502fd93abd69ab10466d
Diego Marquez <diegomarquezp@google.com>
2023-01-04 22:24:02 +0000

    remove wrapper in cleanup.sh

--- spring-cloud-generator/cleanup.sh
+++ spring-cloud-generator/cleanup.sh
@@ -4,3 +4,4 @@ git checkout HEAD -- ../spring-cloud-gcp-starters
 git checkout HEAD -- ../spring-cloud-previews
 rm -rdf ../spring-cloud-previews/google*
 rm -rdf gapic-generator-java googleapis
+rm ../.mvn/wrapper/maven-wrapper.jar
