e70d293f218dfbcf956f0c76647327e020bc3dab
Diego Marquez <diegomarquezp@google.com>
2023-01-04 22:15:36 +0000

    fix: remove unnecessary commented `set -x`

--- spring-cloud-generator/generate-one.sh
+++ spring-cloud-generator/generate-one.sh
@@ -7,8 +7,6 @@ set -e
 # poc with one specified repo - vision
 #cmd line:: ./generate-one.sh -c vision -v 3.1.2 -i google-cloud-vision -g com.google.cloud -p 3.5.0-SNAPSHOT -d 1
 
-#set -x
-
 # by default, do not download repos
 download_repos=0
 while getopts c:v:i:g:d:p:f: flag
