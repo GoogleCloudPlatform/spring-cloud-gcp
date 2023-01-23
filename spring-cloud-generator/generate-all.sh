42f7b2f914801d09fd35efd4995ed308487c1a5e
Diego Alonso Marquez Palacios <diegomarquezp@google.com>
2023-01-23 11:43:19 -0500

    chore(codegen): add link to client library documentation in previews README.md (#1501)
    * feat: add link to client library documentation in previews README.md
    
    * fix: use concise library description in README
    
    Co-authored-by: Emily Wang <emmwang@google.com>
    
    * chore: adapt column headers
    
    * use -m flag for `monorepo_folder` in `gen-one`
    
    * use `-m` flag in `generate-all.sh`
    
    Co-authored-by: Emily Wang <emmwang@google.com>

--- spring-cloud-generator/generate-all.sh
+++ spring-cloud-generator/generate-all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 WORKING_DIR=`pwd`
 
+mkdir -p run-sanity-check
+touch run-sanity-check/generate-all-started
+monorepo_commitish="v$(bash compute-monorepo-tag.sh)"
+
 cd ../
 # Compute the project version.
 PROJECT_VERSION=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
@@ -19,7 +23,7 @@ save_error_info () {
 
 bash download-repos.sh
 libraries=$(cat $WORKING_DIR/library_list.txt | tail -n+2)
-while IFS=, read -r library_name googleapis_location coordinates_version googleapis_commitish; do
+while IFS=, read -r library_name googleapis_location coordinates_version googleapis_commitish monorepo_folder; do
   echo "processing library $library_name"
   group_id=$(echo $coordinates_version | cut -f1 -d:)
   artifact_id=$(echo $coordinates_version | cut -f2 -d:)
@@ -33,7 +37,9 @@ while IFS=, read -r library_name googleapis_location coordinates_version googlea
     -g $group_id \
     -p $PROJECT_VERSION \
     -f $googleapis_location \
-    -x $googleapis_commitish 2>&1 | tee tmp-generate-one-output || save_error_info $library_name
+    -m $monorepo_folder \
+    -x $googleapis_commitish \
+    -z $monorepo_commitish 2>&1 | tee tmp-generate-one-output || save_error_info $library_name
   set +o pipefail
 done <<< $libraries
 
