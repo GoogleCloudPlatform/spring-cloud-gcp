731923af4f40427e3cc083b36b3a7de5ce092f4c
Emily Wang <emmwang@google.com>
2023-01-24 13:54:56 -0500

    Remove formatting change to unit test file

--- spring-cloud-previews/google-cloud-language-spring-starter/src/test/java/com/google/cloud/language/v1/spring/LanguageAutoConfigurationTests.java
+++ spring-cloud-previews/google-cloud-language-spring-starter/src/test/java/com/google/cloud/language/v1/spring/LanguageAutoConfigurationTests.java
@@ -50,8 +50,7 @@ class LanguageAutoConfigurationTests {
   private static final String SERVICE_CREDENTIAL_CLIENT_ID = "45678";
   private static final String TOP_LEVEL_CREDENTIAL_CLIENT_ID = "12345";
   private static final String SERVICE_OVERRIDE_CLIENT_ID = "56789";
-  private static final String TRANSPORT_CHANNEL_PROVIDER_QUALIFIER_NAME =
-      "defaultLanguageServiceTransportChannelProvider";
+  private static final String TRANSPORT_CHANNEL_PROVIDER_QUALIFIER_NAME = "defaultLanguageServiceTransportChannelProvider";
 
   @Mock private TransportChannel mockTransportChannel;
   @Mock private ApiCallContext mockApiCallContext;
@@ -111,9 +110,10 @@ class LanguageAutoConfigurationTests {
             () -> mockTransportChannelProvider)
         .run(
             ctx -> {
-              assertThat(ctx.getBeanNamesForType(TransportChannelProvider.class))
-                  .containsExactlyInAnyOrder(
-                      "anotherTransportChannelProvider", TRANSPORT_CHANNEL_PROVIDER_QUALIFIER_NAME);
+              assertThat(ctx.getBeanNamesForType(
+                  TransportChannelProvider.class)).containsExactlyInAnyOrder(
+                  "anotherTransportChannelProvider",
+                  TRANSPORT_CHANNEL_PROVIDER_QUALIFIER_NAME);
               LanguageServiceClient client = ctx.getBean(LanguageServiceClient.class);
               TransportChannelProvider transportChannelProviderBean =
                   (TransportChannelProvider) ctx.getBean(TRANSPORT_CHANNEL_PROVIDER_QUALIFIER_NAME);
@@ -123,6 +123,7 @@ class LanguageAutoConfigurationTests {
               assertThat(transportChannelProvider).isNotSameAs(mockTransportChannelProvider);
             });
   }
+
 
   @Test
   void testShouldUseDefaultTransportChannelProvider() {
