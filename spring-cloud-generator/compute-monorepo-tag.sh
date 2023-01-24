3a84805b58f59128bab3cb2b5d4fe51a284a9ff1
Emily Wang <emmwang@google.com>
2023-01-24 16:31:51 -0500

    temp: monorepo committish override for 1.3.0

--- spring-cloud-generator/compute-monorepo-tag.sh
+++ spring-cloud-generator/compute-monorepo-tag.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-libraries_bom_version=$(xmllint --xpath "string(//*[local-name()='gcp-libraries-bom.version'])" ../spring-cloud-gcp-dependencies/pom.xml)
-gapic_libraries_groupId='com.google.cloud'
-gapic_libraries_artifactId='gapic-libraries-bom'
-curl -s "https://raw.githubusercontent.com/googleapis/java-cloud-bom/libraries-bom-v$libraries_bom_version/google-cloud-bom/pom.xml" > libraries-bom-pom
-monorepo_version=$(xmllint --xpath "string(//*[local-name()='dependencies']/*[local-name()='dependency'][*[local-name()='groupId']='$gapic_libraries_groupId'][*[local-name()='artifactId']='$gapic_libraries_artifactId']/*[local-name()='version'])" libraries-bom-pom)
-rm libraries-bom-pom
-echo $monorepo_version
+#libraries_bom_version=$(xmllint --xpath "string(//*[local-name()='gcp-libraries-bom.version'])" ../spring-cloud-gcp-dependencies/pom.xml)
+#gapic_libraries_groupId='com.google.cloud'
+#gapic_libraries_artifactId='gapic-libraries-bom'
+#curl -s "https://raw.githubusercontent.com/googleapis/java-cloud-bom/libraries-bom-v$libraries_bom_version/google-cloud-bom/pom.xml" > libraries-bom-pom
+#monorepo_version=$(xmllint --xpath "string(//*[local-name()='dependencies']/*[local-name()='dependency'][*[local-name()='groupId']='$gapic_libraries_groupId'][*[local-name()='artifactId']='$gapic_libraries_artifactId']/*[local-name()='version'])" libraries-bom-pom)
+#rm libraries-bom-pom
+#echo $monorepo_version
+echo "1.3.0"
