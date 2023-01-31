b958d94997ffdda81ffb97bab150ffc5471e25cc
ddixit14 <deepankardixit@google.com>
2023-01-31 12:17:29 -0500

    chore: changing image to java11 and removing pyenv usage

--- .kokoro/publish_javadoc.sh
+++ .kokoro/publish_javadoc.sh
@@ -5,9 +5,6 @@ if [[ -z "${CREDENTIALS}" ]]; then
   CREDENTIALS=${KOKORO_KEYSTORE_DIR}/73713_docuploader_service_account
 fi
 
-pyenv install 3.7.2
-pyenv global 3.7.2
-
 # Get into the spring-cloud-gcp repo directory
 dir=$(dirname "$0")
 pushd $dir/../
@@ -21,19 +18,19 @@ python3 -m pip install --upgrade protobuf
 python3 -m pip install gcp-docuploader
 
 # Build the javadocs
-./mvnw clean javadoc:aggregate -Drelease=true
-
-# Move into generated docs directory
-pushd target/site/apidocs/
-
-python3 -m docuploader create-metadata \
-     --name spring-cloud-gcp \
-     --version ${PROJECT_VERSION} \
-     --language java
-
-python3 -m docuploader upload . \
-     --credentials ${CREDENTIALS} \
-     --staging-bucket docs-staging
+#./mvnw clean javadoc:aggregate -Drelease=true
+#
+## Move into generated docs directory
+#pushd target/site/apidocs/
+#
+#python3 -m docuploader create-metadata \
+#     --name spring-cloud-gcp \
+#     --version ${PROJECT_VERSION} \
+#     --language java
+#
+#python3 -m docuploader upload . \
+#     --credentials ${CREDENTIALS} \
+#     --staging-bucket docs-staging
 
 popd
 popd
