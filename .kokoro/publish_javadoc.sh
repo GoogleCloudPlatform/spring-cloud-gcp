72e822cf6b7b39531fe5d9f212a2c6c8be89174d
Deepankar Dixit <90280028+ddixit14@users.noreply.github.com>
2023-01-31 14:02:30 -0500

    chore: update publish_javadoc.sh (#1553)
    * chore: update publish_javadoc.sh

--- .kokoro/publish_javadoc.sh
+++ .kokoro/publish_javadoc.sh
@@ -5,14 +5,9 @@ if [[ -z "${CREDENTIALS}" ]]; then
   CREDENTIALS=${KOKORO_KEYSTORE_DIR}/73713_docuploader_service_account
 fi
 
-pyenv global 3.7.2
-
 # Get into the spring-cloud-gcp repo directory
 dir=$(dirname "$0")
 pushd $dir/../
-
-# change to release version
-./mvnw versions:set --batch-mode -DremoveSnapshot -DprocessAllModules
 
 # Compute the project version.
 PROJECT_VERSION=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
@@ -25,17 +20,17 @@ python3 -m pip install gcp-docuploader
 # Build the javadocs
 ./mvnw clean javadoc:aggregate -Drelease=true
 
-# Move into generated docs directory
+## Move into generated docs directory
 pushd target/site/apidocs/
 
 python3 -m docuploader create-metadata \
-    --name spring-cloud-gcp \
-    --version ${PROJECT_VERSION} \
-    --language java
+     --name spring-cloud-gcp \
+     --version ${PROJECT_VERSION} \
+     --language java
 
 python3 -m docuploader upload . \
-    --credentials ${CREDENTIALS} \
-    --staging-bucket docs-staging
+     --credentials ${CREDENTIALS} \
+     --staging-bucket docs-staging
 
 popd
 popd
