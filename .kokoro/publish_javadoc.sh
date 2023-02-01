552f358b1de7afe2c10d10ad66955a0360effe72
ddixit14 <deepankardixit@google.com>
2023-02-01 16:17:03 -0500

    fetching project version from versions.txt

--- .kokoro/publish_javadoc.sh
+++ .kokoro/publish_javadoc.sh
@@ -5,32 +5,30 @@ if [[ -z "${CREDENTIALS}" ]]; then
   CREDENTIALS=${KOKORO_KEYSTORE_DIR}/73713_docuploader_service_account
 fi
 
-# Get into the spring-cloud-gcp repo directory
+## Get into the spring-cloud-gcp repo directory
 dir=$(dirname "$0")
 pushd $dir/../
 
 # Compute the project version.
-PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-
-echo "the project version is : ${PROJECT_VERSION}"
+PROJECT_VERSION=$(grep "^spring-cloud-gcp:" "./versions.txt" | cut -d: -f3)
 
 # install docuploader package
-#python3 -m pip install --require-hashes -r .kokoro/requirements.txt
-#
-## Build the javadocs
-#mvn clean javadoc:aggregate -Drelease=true
-#
-### Move into generated docs directory
-#pushd target/site/apidocs/
-#
-#python3 -m docuploader create-metadata \
-#     --name spring-cloud-gcp \
-#     --version ${PROJECT_VERSION} \
-#     --language java
-#
-#python3 -m docuploader upload . \
-#     --credentials ${CREDENTIALS} \
-#     --staging-bucket docs-staging
-#
-#popd
+python3 -m pip install --require-hashes -r .kokoro/requirements.txt
+
+# Build the javadocs
+mvn clean javadoc:aggregate -Drelease=true
+
+# Move into generated docs directory
+pushd target/site/apidocs/
+
+python3 -m docuploader create-metadata \
+     --name spring-cloud-gcp \
+     --version ${PROJECT_VERSION} \
+     --language java
+
+python3 -m docuploader upload . \
+     --credentials ${CREDENTIALS} \
+     --staging-bucket docs-staging
+
+popd
 popd
