fe5e0efa0f4cabc6278558a311c15be9022133c7
Joe Wang <joewa@google.com>
2023-02-02 11:38:31 -0500

    remove default property

--- spring-cloud-gcp-autoconfigure/src/test/java/com/google/cloud/spring/autoconfigure/secretmanager/SecretManagerCompatibilityTests.java
+++ spring-cloud-gcp-autoconfigure/src/test/java/com/google/cloud/spring/autoconfigure/secretmanager/SecretManagerCompatibilityTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.cloud.spring.autoconfigure.secretmanager;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -66,14 +82,12 @@ class SecretManagerCompatibilityTests {
    */
   @Test
   void testConfigurationWhenDefaultSecretIsNotAllowed() {
-    application.properties(
-            "spring.config.import=sm://")
-        .addBootstrapRegistryInitializer(
+    application.addBootstrapRegistryInitializer(
             (registry) -> registry.registerIfAbsent(
                 SecretManagerServiceClient.class,
                 InstanceSupplier.of(client)
             )
-        );
+    );
     try (ConfigurableApplicationContext applicationContext = application.run()) {
       ConfigurableEnvironment environment = applicationContext.getEnvironment();
       assertThat(environment.getProperty("sm://my-secret")).isEqualTo("newSecret");
@@ -85,8 +99,7 @@ class SecretManagerCompatibilityTests {
   @Test
   void testConfigurationWhenDefaultSecretIsAllowed() {
     application.properties(
-            "spring.cloud.gcp.secretmanager.allow-default-secret=true",
-            "spring.config.import=sm://")
+            "spring.cloud.gcp.secretmanager.allow-default-secret=true")
         .addBootstrapRegistryInitializer(
             (registry) -> registry.registerIfAbsent(
                 SecretManagerServiceClient.class,
