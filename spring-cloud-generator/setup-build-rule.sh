5174d06e2b49bcf1230232b79d35e40d35641b97
Emily Wang <emmwang@google.com>
2023-03-28 16:03:32 -0400

    chore: switch BUILD.bazel modifications to use buildozer

--- spring-cloud-generator/setup-build-rule.sh
+++ spring-cloud-generator/setup-build-rule.sh
@@ -24,14 +24,14 @@ git checkout $googleapis_commitish -- $googleapis_folder
 # Additional rule to load
 SPRING_RULE_NAME="    \\\"java_gapic_spring_library\\\","
 perl -0777 -pi -e "s/(load\((.*?)\"java_gapic_library\",)/\$1\n$SPRING_RULE_NAME/s" $googleapis_folder/BUILD.bazel
-# Duplicate java_gapic_library rule definition
-perl -0777 -pi -e "s/(java_gapic_library\((.*?)\))/\$1\n\n\$1/s" $googleapis_folder/BUILD.bazel
-# Update rule name to java_gapic_spring_library
-perl -0777 -pi -e "s/(java_gapic_library\()/java_gapic_spring_library\(/s" $googleapis_folder/BUILD.bazel
-# Update name argument to have _spring appended
-perl -0777 -pi -e "s/(java_gapic_spring_library\((.*?)name = \"(.*?)\")/java_gapic_spring_library\(\$2name = \"\$3_spring\"/s" $googleapis_folder/BUILD.bazel
-# todo: better way to remove the following unused arguments?
-perl -0777 -pi -e "s/(java_gapic_spring_library\((.*?)(\n    test_deps = \[(.*?)\](.*?),))/java_gapic_spring_library\(\$2/s" $googleapis_folder/BUILD.bazel
-perl -0777 -pi -e "s/(java_gapic_spring_library\((.*?)(\n    deps = \[(.*?)\](.*?),))/java_gapic_spring_library\(\$2/s" $googleapis_folder/BUILD.bazel
-perl -0777 -pi -e "s/(java_gapic_spring_library\((.*?)(\n    rest_numeric_enums = (.*?),))/java_gapic_spring_library\(\$2/s" $googleapis_folder/BUILD.bazel
 
+# Add java_gapic_spring_library rule, with attrs copied from corresponding java_gapic_library rule
+SPRING_RULE_NAME="$(buildozer 'print name' $googleapis_folder/BUILD.bazel:%java_gapic_library)_spring"
+
+buildozer "new java_gapic_spring_library $SPRING_RULE_NAME" $googleapis_folder/BUILD.bazel:__pkg__
+# Copy attributes from java_gapic_library rule
+buildozer "copy srcs language_java_gapic" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
+buildozer "copy grpc_service_config language_java_gapic" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
+buildozer "copy gapic_yaml language_java_gapic" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
+buildozer "copy service_yaml language_java_gapic" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
+buildozer "copy transport language_java_gapic" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
