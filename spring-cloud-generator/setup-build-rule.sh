8a9fe0d65873d8e3f84a21315654f3d3c2e06ae1
Emily Wang <emmwang@google.com>
2023-03-28 17:27:46 -0400

    chore: fix references to java_gapic_library rule name

--- spring-cloud-generator/setup-build-rule.sh
+++ spring-cloud-generator/setup-build-rule.sh
@@ -26,12 +26,13 @@ SPRING_RULE_NAME="    \\\"java_gapic_spring_library\\\","
 perl -0777 -pi -e "s/(load\((.*?)\"java_gapic_library\",)/\$1\n$SPRING_RULE_NAME/s" $googleapis_folder/BUILD.bazel
 
 # Add java_gapic_spring_library rule, with attrs copied from corresponding java_gapic_library rule
-SPRING_RULE_NAME="$(buildozer 'print name' $googleapis_folder/BUILD.bazel:%java_gapic_library)_spring"
+GAPIC_RULE_NAME="$(buildozer 'print name' $googleapis_folder/BUILD.bazel:%java_gapic_library)"
+SPRING_RULE_NAME="${GAPIC_RULE_NAME}_spring"
 
 buildozer "new java_gapic_spring_library $SPRING_RULE_NAME" $googleapis_folder/BUILD.bazel:__pkg__
 # Copy attributes from java_gapic_library rule
-buildozer "copy srcs language_java_gapic" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
-buildozer "copy grpc_service_config language_java_gapic" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
-buildozer "copy gapic_yaml language_java_gapic" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
-buildozer "copy service_yaml language_java_gapic" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
-buildozer "copy transport language_java_gapic" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
+buildozer "copy srcs $GAPIC_RULE_NAME" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
+buildozer "copy grpc_service_config $GAPIC_RULE_NAME" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
+buildozer "copy gapic_yaml $GAPIC_RULE_NAME" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
+buildozer "copy service_yaml $GAPIC_RULE_NAME" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
+buildozer "copy transport $GAPIC_RULE_NAME" $googleapis_folder/BUILD.bazel:$SPRING_RULE_NAME
