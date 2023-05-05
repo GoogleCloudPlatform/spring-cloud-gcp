562ceb29026e05511c4fa00ef3c39a1cb8c72ce0
Diego Marquez <diegomarquezp@google.com>
2023-05-05 19:21:12 +0000

    chore: fix filename reference for temp output

--- spring-cloud-generator/scripts/generate-all.sh
+++ spring-cloud-generator/scripts/generate-all.sh
@@ -24,7 +24,7 @@ cd ${WORKING_DIR}
 # args 1 - library name;
 save_error_info () {
   mkdir -p ${WORKING_DIR}/failed-library-generations
-  mv tmp-generate-one-output ${WORKING_DIR}/failed-library-generations/$1
+  mv tmp-output ${WORKING_DIR}/failed-library-generations/$1
 }
 
 # runs generate-one.sh for each entry in library_list.txt
