eaf2ef5c0ea226d4f5ec45da090d0fbff01a675e
Emily Wang <emmwang@google.com>
2023-05-25 14:53:01 -0400

    chore: remove unused RestTestProtoLoader import

--- spring-cloud-generator/src/test/java/com/google/cloud/generator/spring/composer/SpringAutoConfigClassComposerTest.java
+++ spring-cloud-generator/src/test/java/com/google/cloud/generator/spring/composer/SpringAutoConfigClassComposerTest.java
@@ -24,7 +24,6 @@ import com.google.api.generator.gapic.model.Service;
 import com.google.api.generator.gapic.model.Transport;
 import com.google.api.generator.test.framework.Assert;
 import com.google.api.generator.test.protoloader.GrpcRestTestProtoLoader;
-import com.google.api.generator.test.protoloader.RestTestProtoLoader;
 import com.google.api.generator.test.protoloader.TestProtoLoader;
 import org.junit.Before;
 import org.junit.Test;
