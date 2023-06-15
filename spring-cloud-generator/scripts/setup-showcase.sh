9af0fad664437c5f2bc9d7f672456086ef3e7726
Emily Wang <emmwang@google.com>
2023-06-15 10:15:59 -0400

    chore: update setup script

--- spring-cloud-generator/scripts/setup-showcase.sh
+++ spring-cloud-generator/scripts/setup-showcase.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-WORKING_DIR=`pwd` # spring-cloud-generator
+# If not set, assume working directory is spring-cloud-generator
+if [[ -z "$SPRING_GENERATOR_DIR" ]]; then
+  SPRING_GENERATOR_DIR=`pwd`
+fi
+SPRING_ROOT_DIR=${SPRING_GENERATOR_DIR}/..
 
 # Compute the parent project version.
-PROJECT_VERSION=$(./${WORKING_DIR}/../mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
+cd ${SPRING_ROOT_DIR}
+PROJECT_VERSION=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 #Alternative: clone googleapis and gapic-showcase instead of sdk-platform-java
 #git clone https://github.com/googleapis/googleapis.git
 #git clone https://github.com/googleapis/gapic-showcase.git
 
 # Install local snapshot jar for spring generator
-cd ${WORKING_DIR} && mvn install
+# TODO(emmwang): this might need an install of spring-cloud-gcp from root
+cd ${SPRING_GENERATOR_DIR} && mvn install
 
 # Clone sdk-platform-java with showcase library for testing
 git clone https://github.com/googleapis/sdk-platform-java.git
@@ -18,6 +24,7 @@ git clone https://github.com/googleapis/sdk-platform-java.git
 # git checkout ${GAPIC_GENERATOR_JAVA_VERSION}
 
 # Install showcase client libraries locally
+# TODO(emmwang): this might need an install of sdk-platform-java from root
 cd sdk-platform-java/showcase && mvn clean install
 
 # Modify sdk-platform-java/WORKSPACE 0 adapted from setup-googleapi-rules.sh
@@ -53,9 +60,9 @@ bazelisk build --tool_java_language_version=17 --tool_java_runtime_version=remot
 # Post-process generated modules - adapted from generate-one.sh
 # Copy and unzip _java_gapic_spring-spring.srcjar
 ## copy spring code to outside
-mkdir -p ${WORKING_DIR}/showcase
-cp ../bazel-bin/showcase/showcase_java_gapic_spring-spring.srcjar ${WORKING_DIR}/showcase
-cd ${WORKING_DIR}/showcase
+mkdir -p ${SPRING_GENERATOR_DIR}/showcase
+cp ../bazel-bin/showcase/showcase_java_gapic_spring-spring.srcjar ${SPRING_GENERATOR_DIR}/showcase
+cd ${SPRING_GENERATOR_DIR}/showcase
 unzip -o showcase_java_gapic_spring-spring.srcjar -d showcase-spring-starter/
 rm -rf showcase_java_gapic_spring-spring.srcjar
 
@@ -71,5 +78,9 @@ sed -i 's/^ *<relativePath>.*/'"$RELATIVE_PATH"'/g' showcase-spring-starter/pom.
 
 # Run google-java-format on generated code
 cd showcase-spring-starter
-./${WORKING_DIR}/../mvnw com.coveo:fmt-maven-plugin:format -Dfmt.skip=false
+./../../../mvnw com.coveo:fmt-maven-plugin:format -Dfmt.skip=false
+
+# Remove downloaded repos
+cd ${SPRING_GENERATOR_DIR}
+rm -rf sdk-platform-java
 
