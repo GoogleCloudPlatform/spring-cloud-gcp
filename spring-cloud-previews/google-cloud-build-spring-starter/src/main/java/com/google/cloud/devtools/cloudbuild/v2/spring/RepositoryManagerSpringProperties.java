215add469321a99657503a0a107aa328545534c9
mehdi0933 <mehdibenhalla2012@gmail.com>
2023-07-24 18:32:59 -0400

    chore: update starter modules in spring-cloud-previews (#2054)
    Co-authored-by: Cloud Java Bot <cloud-java-bot@google.com>

--- spring-cloud-previews/google-cloud-build-spring-starter/src/main/java/com/google/cloud/devtools/cloudbuild/v2/spring/RepositoryManagerSpringProperties.java
+++ spring-cloud-previews/google-cloud-build-spring-starter/src/main/java/com/google/cloud/devtools/cloudbuild/v2/spring/RepositoryManagerSpringProperties.java
@@ -78,6 +78,11 @@ public class RepositoryManagerSpringProperties implements CredentialsSupplier {
    */
   @NestedConfigurationProperty private Retry fetchLinkableRepositoriesRetry;
   /**
+   * Allow override of retry settings at method-level for fetchGitRefs. If defined, this takes
+   * precedence over service-level retry configurations for that RPC method.
+   */
+  @NestedConfigurationProperty private Retry fetchGitRefsRetry;
+  /**
    * Allow override of retry settings at method-level for setIamPolicy. If defined, this takes
    * precedence over service-level retry configurations for that RPC method.
    */
@@ -184,6 +189,14 @@ public class RepositoryManagerSpringProperties implements CredentialsSupplier {
 
   public void setFetchLinkableRepositoriesRetry(Retry fetchLinkableRepositoriesRetry) {
     this.fetchLinkableRepositoriesRetry = fetchLinkableRepositoriesRetry;
+  }
+
+  public Retry getFetchGitRefsRetry() {
+    return this.fetchGitRefsRetry;
+  }
+
+  public void setFetchGitRefsRetry(Retry fetchGitRefsRetry) {
+    this.fetchGitRefsRetry = fetchGitRefsRetry;
   }
 
   public Retry getSetIamPolicyRetry() {
