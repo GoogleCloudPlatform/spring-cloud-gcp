aeddcab59871b8d467faee7e6ce0621657b0cd59
Lawrence Qiu <lawrenceqiu@google.com>
2023-08-09 13:36:51 -0400

    chore: Revert spring version bump

--- docs/src/main/asciidoc/ghpages.sh
+++ docs/src/main/asciidoc/ghpages.sh
@@ -60,7 +60,7 @@ function switch_to_tag() {
 # Build the docs if switch is on
 function build_docs_if_applicable() {
     if [[ "${BUILD}" == "yes" ]] ; then
-        ./mvnw clean install -DskipTests -T 1C
+        ./mvnw clean install -DskipTests -T 1C -q
         echo "Running the build docs script"
         ./mvnw install -P docs -pl docs -DskipTests -q
         ls -la docs/
