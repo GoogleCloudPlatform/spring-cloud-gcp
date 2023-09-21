792be6fca44793856fffbca5ce4751ce50193b8c
mpeddada1 <mpeddada@google.com>
2023-09-21 01:10:43 +0000

    update script

--- .github/workflows/scripts/native-image-tests.sh
+++ .github/workflows/scripts/native-image-tests.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-if [ -z "$MODULE_TO_TEST" ]; then
-  echo "Please specify the MODULE_TO_TEST."
+if [ -z "$MODULE_UNDER_TEST" ]; then
+  echo "Please specify the MODULE_UNDER_TEST."
   exit 1
 fi
 
-case ${MODULE_TO_TEST} in
-  vision)
-    pl="spring-cloud-gcp-vision"
-    ;;
-  storage)
-    pl="spring-cloud-gcp-storage"
-    ;;
-  spanner)
-     pl="spring-cloud-gcp-spanner"
-    ;;
-  *)
-    pl=""
-    ;;
-esac
+if [[ "$MODULE_UNDER_TEST" = "datastore" || "$MODULE_UNDER_TEST" = "firestore" || "$MODULE_UNDER_TEST" = "spanner" ]]; then
+  testDirectory="spring-cloud-gcp-data-${MODULE_UNDER_TEST}"
+else
+  testDirectory="spring-cloud-gcp-${MODULE_UNDER_TEST}"
+fi
 
-mvn verify -Pspring-native,!default -pl="${pl}"
+mvn verify -Pspring-native,!default -pl="${testDirectory}"
