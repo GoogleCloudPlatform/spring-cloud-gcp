7c454bd55268ed8167e1d2657a91ba33e0b4edbf
Prateek Jain <jainsahab@google.com>
2023-10-12 15:56:44 +0530

    Isolation and Propagation validation in transaction manager

--- spring-cloud-spanner-spring-data-r2dbc/src/main/java/com/google/cloud/spanner/r2dbc/springdata/SpannerReactiveTransactionManager.java
+++ spring-cloud-spanner-spring-data-r2dbc/src/main/java/com/google/cloud/spanner/r2dbc/springdata/SpannerReactiveTransactionManager.java
@@ -1,10 +1,24 @@
 package com.google.cloud.spanner.r2dbc.springdata;
 
+import static org.springframework.transaction.TransactionDefinition.ISOLATION_READ_COMMITTED;
+import static org.springframework.transaction.TransactionDefinition.ISOLATION_READ_UNCOMMITTED;
+import static org.springframework.transaction.TransactionDefinition.ISOLATION_REPEATABLE_READ;
+import static org.springframework.transaction.TransactionDefinition.PROPAGATION_NESTED;
+
 import com.google.cloud.spanner.TimestampBound;
 import io.r2dbc.spi.TransactionDefinition;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.r2dbc.connection.R2dbcTransactionManager;
+import org.springframework.transaction.reactive.TransactionSynchronizationManager;
+import reactor.core.publisher.Mono;
 
 public class SpannerReactiveTransactionManager extends R2dbcTransactionManager {
+
+  private final static List<Integer> UNSUPPORTED_ISOLATION_LEVELS = Arrays.asList(
+      ISOLATION_READ_UNCOMMITTED,
+      ISOLATION_READ_COMMITTED,
+      ISOLATION_REPEATABLE_READ);
 
   private final TimestampBound timestampBound;
 
@@ -14,6 +28,20 @@ public class SpannerReactiveTransactionManager extends R2dbcTransactionManager {
 
   public SpannerReactiveTransactionManager() {
     this.timestampBound = TimestampBound.strong();
+  }
+
+  @Override
+  protected Mono<Void> doBegin(TransactionSynchronizationManager synchronizationManager,
+      Object transaction, org.springframework.transaction.TransactionDefinition definition) {
+    if (UNSUPPORTED_ISOLATION_LEVELS.contains(definition.getIsolationLevel())) {
+      return Mono.error(new UnsupportedOperationException(
+          "Only ISOLATION_DEFAULT and ISOLATION_SERIALIZABLE are supported."));
+    }
+    if (PROPAGATION_NESTED == definition.getPropagationBehavior()) {
+      return Mono.error(new UnsupportedOperationException(
+          "PROPAGATION_NESTED behaviour is not supported."));
+    }
+    return super.doBegin(synchronizationManager, transaction, definition);
   }
 
   @Override
--- spring-cloud-spanner-spring-data-r2dbc/src/test/java/com/google/cloud/spanner/r2dbc/springdata/SpannerReactiveTransactionManagerTest.java
+++ spring-cloud-spanner-spring-data-r2dbc/src/test/java/com/google/cloud/spanner/r2dbc/springdata/SpannerReactiveTransactionManagerTest.java
@@ -13,7 +13,17 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.transaction.TransactionDefinition.ISOLATION_READ_COMMITTED;
+import static org.springframework.transaction.TransactionDefinition.ISOLATION_READ_UNCOMMITTED;
+import static org.springframework.transaction.TransactionDefinition.ISOLATION_REPEATABLE_READ;
 import static org.springframework.transaction.TransactionDefinition.ISOLATION_SERIALIZABLE;
+import static org.springframework.transaction.TransactionDefinition.PROPAGATION_MANDATORY;
+import static org.springframework.transaction.TransactionDefinition.PROPAGATION_NESTED;
+import static org.springframework.transaction.TransactionDefinition.PROPAGATION_NEVER;
+import static org.springframework.transaction.TransactionDefinition.PROPAGATION_NOT_SUPPORTED;
+import static org.springframework.transaction.TransactionDefinition.PROPAGATION_REQUIRED;
+import static org.springframework.transaction.TransactionDefinition.PROPAGATION_REQUIRES_NEW;
+import static org.springframework.transaction.TransactionDefinition.PROPAGATION_SUPPORTS;
 
 import com.google.cloud.spanner.TimestampBound;
 import io.r2dbc.spi.Connection;
@@ -31,6 +41,7 @@ import org.springframework.r2dbc.connection.R2dbcTransactionManager;
 import org.springframework.transaction.reactive.TransactionContextManager;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SpannerReactiveTransactionManagerTest {
@@ -39,11 +50,14 @@ public class SpannerReactiveTransactionManagerTest {
   private ConnectionFactory connectionFactory;
   @Mock
   private Connection connection;
+  private R2dbcTransactionManager reactiveTransactionManager;
 
   @Before
   public void setUp() {
     doReturn(Mono.just(connection)).when(connectionFactory).create();
     when(connection.beginTransaction(any())).thenReturn(Mono.empty());
+    reactiveTransactionManager = new SpannerReactiveTransactionManager();
+    reactiveTransactionManager.setConnectionFactory(connectionFactory);
   }
 
   @Test
@@ -68,18 +82,14 @@ public class SpannerReactiveTransactionManagerTest {
     assertThat(actualTransactionDefinition.getAttribute(ISOLATION_LEVEL), is(SERIALIZABLE));
     assertThat(actualTransactionDefinition.getAttribute(READ_ONLY), is(false));
     assertThat(actualTransactionDefinition.getAttribute(NAME), is("custom name"));
-    assertThat(actualTransactionDefinition.getAttribute(LOCK_WAIT_TIMEOUT), is(Duration.of(5, SECONDS)));
+    assertThat(actualTransactionDefinition.getAttribute(LOCK_WAIT_TIMEOUT),
+        is(Duration.of(5, SECONDS)));
     // spanner specific attribute assertion
     assertThat(actualTransactionDefinition.getAttribute(TIMESTAMP_BOUND), is(fiveSecondStaleness));
-
   }
 
   @Test
   public void shouldIncludeDefaultCustomTimestampBoundInTransactionDefinitionDuringBeginTransaction() {
-    // setup
-    R2dbcTransactionManager reactiveTransactionManager = new SpannerReactiveTransactionManager();
-    reactiveTransactionManager.setConnectionFactory(connectionFactory);
-
     // action
     reactiveTransactionManager.getReactiveTransaction(getTransactionDefinition())
         .contextWrite(TransactionContextManager.getOrCreateContext())
@@ -94,18 +104,119 @@ public class SpannerReactiveTransactionManagerTest {
     assertThat(actualTransactionDefinition.getAttribute(ISOLATION_LEVEL), is(SERIALIZABLE));
     assertThat(actualTransactionDefinition.getAttribute(READ_ONLY), is(false));
     assertThat(actualTransactionDefinition.getAttribute(NAME), is("custom name"));
-    assertThat(actualTransactionDefinition.getAttribute(LOCK_WAIT_TIMEOUT), is(Duration.of(5, SECONDS)));
+    assertThat(actualTransactionDefinition.getAttribute(LOCK_WAIT_TIMEOUT),
+        is(Duration.of(5, SECONDS)));
     // spanner specific attribute assertion
-    assertThat(actualTransactionDefinition.getAttribute(TIMESTAMP_BOUND), is(TimestampBound.strong()));
+    assertThat(actualTransactionDefinition.getAttribute(TIMESTAMP_BOUND),
+        is(TimestampBound.strong()));
 
   }
 
+  @Test
+  public void shouldValidateIsolationDuringBeginTransaction() {
+    StepVerifier.create(
+            reactiveTransactionManager.getReactiveTransaction(
+                    transactionDefinitionWithIsolation(ISOLATION_SERIALIZABLE)) // ISOLATION_SERIALIZABLE
+                .contextWrite(TransactionContextManager.getOrCreateContext())
+                .contextWrite(TransactionContextManager.getOrCreateContextHolder()))
+        .expectSubscription().expectComplete();
+
+    StepVerifier.create(
+            reactiveTransactionManager.getReactiveTransaction(
+                    transactionDefinitionWithIsolation(ISOLATION_READ_UNCOMMITTED)) // ISOLATION_READ_UNCOMMITTED
+                .contextWrite(TransactionContextManager.getOrCreateContext())
+                .contextWrite(TransactionContextManager.getOrCreateContextHolder()))
+        .expectError(UnsupportedOperationException.class).verify();
+
+    StepVerifier.create(
+            reactiveTransactionManager.getReactiveTransaction(
+                    transactionDefinitionWithIsolation(ISOLATION_READ_COMMITTED)) // ISOLATION_READ_COMMITTED
+                .contextWrite(TransactionContextManager.getOrCreateContext())
+                .contextWrite(TransactionContextManager.getOrCreateContextHolder()))
+        .expectError(UnsupportedOperationException.class).verify();
+
+    StepVerifier.create(
+            reactiveTransactionManager.getReactiveTransaction(
+                    transactionDefinitionWithIsolation(ISOLATION_REPEATABLE_READ)) // ISOLATION_REPEATABLE_READ
+                .contextWrite(TransactionContextManager.getOrCreateContext())
+                .contextWrite(TransactionContextManager.getOrCreateContextHolder()))
+        .expectError(UnsupportedOperationException.class).verify();
+  }
+
+  @Test
+  public void shouldValidatePropagationDuringBeginTransaction() {
+    StepVerifier.create(
+            reactiveTransactionManager.getReactiveTransaction(
+                    transactionDefinitionWithPropagation(PROPAGATION_REQUIRED)) // PROPAGATION_REQUIRED
+                .contextWrite(TransactionContextManager.getOrCreateContext())
+                .contextWrite(TransactionContextManager.getOrCreateContextHolder()))
+        .expectSubscription().expectComplete();
+
+    StepVerifier.create(
+            reactiveTransactionManager.getReactiveTransaction(
+                    transactionDefinitionWithPropagation(PROPAGATION_SUPPORTS)) // PROPAGATION_SUPPORTS
+                .contextWrite(TransactionContextManager.getOrCreateContext())
+                .contextWrite(TransactionContextManager.getOrCreateContextHolder()))
+        .expectSubscription().expectComplete();
+
+    StepVerifier.create(
+            reactiveTransactionManager.getReactiveTransaction(
+                    transactionDefinitionWithPropagation(PROPAGATION_MANDATORY)) // PROPAGATION_MANDATORY
+                .contextWrite(TransactionContextManager.getOrCreateContext())
+                .contextWrite(TransactionContextManager.getOrCreateContextHolder()))
+        .expectSubscription().expectComplete();
+
+    StepVerifier.create(
+            reactiveTransactionManager.getReactiveTransaction(
+                    transactionDefinitionWithPropagation(PROPAGATION_REQUIRES_NEW)) // PROPAGATION_REQUIRES_NEW
+                .contextWrite(TransactionContextManager.getOrCreateContext())
+                .contextWrite(TransactionContextManager.getOrCreateContextHolder()))
+        .expectSubscription().expectComplete();
+
+    StepVerifier.create(
+            reactiveTransactionManager.getReactiveTransaction(
+                    transactionDefinitionWithPropagation(PROPAGATION_NOT_SUPPORTED)) // PROPAGATION_NOT_SUPPORTED
+                .contextWrite(TransactionContextManager.getOrCreateContext())
+                .contextWrite(TransactionContextManager.getOrCreateContextHolder()))
+        .expectSubscription().expectComplete();
+
+    StepVerifier.create(
+            reactiveTransactionManager.getReactiveTransaction(
+                    transactionDefinitionWithPropagation(PROPAGATION_NEVER)) // PROPAGATION_NEVER
+                .contextWrite(TransactionContextManager.getOrCreateContext())
+                .contextWrite(TransactionContextManager.getOrCreateContextHolder()))
+        .expectSubscription().expectComplete();
+
+    StepVerifier.create(
+            reactiveTransactionManager.getReactiveTransaction(
+                    transactionDefinitionWithPropagation(PROPAGATION_NESTED)) // PROPAGATION_NESTED
+                .contextWrite(TransactionContextManager.getOrCreateContext())
+                .contextWrite(TransactionContextManager.getOrCreateContextHolder()))
+        .expectError(UnsupportedOperationException.class).verify();
+  }
+
   private static DefaultTransactionDefinition getTransactionDefinition() {
+    return transactionDefinitionWithIsolation(ISOLATION_SERIALIZABLE);
+  }
+
+  private static DefaultTransactionDefinition transactionDefinitionWithIsolation(
+      int isolationLevel) {
+    DefaultTransactionDefinition delegate = new DefaultTransactionDefinition();
+    delegate.setName("custom name");
+    delegate.setIsolationLevel(isolationLevel);
+    delegate.setReadOnly(false);
+    delegate.setTimeout(5);
+    return delegate;
+  }
+
+  private static DefaultTransactionDefinition transactionDefinitionWithPropagation(
+      int propagationBehavior) {
     DefaultTransactionDefinition delegate = new DefaultTransactionDefinition();
     delegate.setName("custom name");
     delegate.setIsolationLevel(ISOLATION_SERIALIZABLE);
     delegate.setReadOnly(false);
     delegate.setTimeout(5);
+    delegate.setPropagationBehavior(propagationBehavior);
     return delegate;
   }
 
