36e1dc5b70b8f3638c265017e1ee5829da338ab9
Min Zhu <zhumin@google.com>
2023-11-15 10:00:48 -0500

    doc: add guide and demo to sample application to explain adding runtime hints for data class with template.

---
+++ spring-cloud-gcp-samples/spring-cloud-gcp-data-firestore-sample/src/main/java/com/example/DataClassRuntimeHints.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import com.google.cloud.spring.data.firestore.FirestoreTemplate;
+import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
+import java.util.Arrays;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
+
+/**
+ * Demonstrates adding runtime hints for native compilation. Runtime hints needs to be registered
+ * for Data POJO classes when desire to use {@link FirestoreTemplate} methods directly without a
+ * repository class for that domain type extending {@link FirestoreReactiveRepository} class.
+ */
+public class DataClassRuntimeHints implements RuntimeHintsRegistrar {
+
+  @Override
+  public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+
+    hints
+        .reflection()
+        .registerTypes(
+            Arrays.asList(TypeReference.of(Person.class)),
+            hint ->
+                hint.withMembers(
+                    MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+                    MemberCategory.INVOKE_DECLARED_METHODS,
+                    MemberCategory.DECLARED_FIELDS));
+
+  }
+}
