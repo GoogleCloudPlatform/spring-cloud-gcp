c29b5c8898ba265c4290bc288845f980f63a060c
Joe Wang <joewa@google.com>
2023-11-18 13:46:01 -0500

    print dir

--- docs/src/main/asciidoc/ghpages.sh
+++ docs/src/main/asciidoc/ghpages.sh
@@ -178,6 +178,7 @@ function copy_docs_for_provided_version() {
     local FOLDER=${DESTINATION_REPO_FOLDER}/${VERSION}
     mkdir -p ${FOLDER}
     echo -e "Current tag is [v${VERSION}] Will copy the current docs to the [${FOLDER}] folder"
+    find ${ROOT_FOLDER}/docs/target -type d
     for f in ${ROOT_FOLDER}/docs/target/generated-docs/*; do
         file=${f#${ROOT_FOLDER}/docs/target/generated-docs/*}
         copy_docs_for_branch ${file} ${FOLDER}
