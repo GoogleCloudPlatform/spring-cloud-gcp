4f117d4aa494d0da00a78817236524c5d1c7d4b5
Joe Wang <joewa@google.com>
2023-11-18 14:15:04 -0500

    print target dir

--- docs/src/main/asciidoc/ghpages.sh
+++ docs/src/main/asciidoc/ghpages.sh
@@ -178,6 +178,7 @@ function copy_docs_for_provided_version() {
     local FOLDER=${DESTINATION_REPO_FOLDER}/${VERSION}
     mkdir -p "${FOLDER}"
     echo -e "Current tag is [v${VERSION}] Will copy the current docs to the [${FOLDER}] folder"
+    find ${ROOT_FOLDER}/docs/target -type d
     while IFS= read -r f; do
         echo "${f}"
         file=${f#${ROOT_FOLDER}/docs/target/generated-docs/}
