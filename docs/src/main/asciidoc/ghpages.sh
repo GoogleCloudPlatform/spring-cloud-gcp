d6ad1cb941cef1f44b38dbcfcbe10bec7ba12916
Joe Wang <joewa@google.com>
2023-11-18 17:26:12 -0500

    restore command

--- docs/src/main/asciidoc/ghpages.sh
+++ docs/src/main/asciidoc/ghpages.sh
@@ -60,7 +60,7 @@ function switch_to_tag() {
 # Build the docs if switch is on
 function build_docs_if_applicable() {
     if [[ "${BUILD}" == "yes" ]] ; then
-        ./mvnw clean org.asciidoctor:asciidoctor-maven-plugin:process-asciidoc install -P docs -pl docs -DskipTests
+        ./mvnw clean install -P docs -pl docs -DskipTests
     fi
 }
 
