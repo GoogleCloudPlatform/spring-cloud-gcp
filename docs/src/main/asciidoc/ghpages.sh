95ee2aaab9789081792f85c564fee6e26deafdef
Joe Wang <joewa@google.com>
2023-11-18 21:05:38 -0500

    change command

--- docs/src/main/asciidoc/ghpages.sh
+++ docs/src/main/asciidoc/ghpages.sh
@@ -62,9 +62,9 @@ function build_docs_if_applicable() {
     if [[ "${BUILD}" == "yes" ]] ; then
         ./mvnw clean \
                org.asciidoctor:asciidoctor-maven-plugin:process-asciidoc \
-               -DsourceDirectory=src/main/asciidoc \
-               -DoutputDirectory=target/generated-docs/reference/html \
-               install -P docs -pl docs -DskipTests
+               -Dasciidoctor.sourceDirectory=src/main/asciidoc \
+               -Dasciidoctor.outputDirectory=target/generated-docs/reference/html \
+               install -P docs -pl docs -DskipTests -X
     fi
 }
 
