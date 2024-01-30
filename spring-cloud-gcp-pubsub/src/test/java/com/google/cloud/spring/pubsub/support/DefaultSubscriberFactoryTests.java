316d4d8a513d926c47d04257d83a78b505209303
Lawrence Qiu <lawrenceqiu@google.com>
2024-01-30 14:00:19 -0500

    chore: Add mock for withEndpointContext

--- spring-cloud-gcp-pubsub/src/test/java/com/google/cloud/spring/pubsub/support/DefaultSubscriberFactoryTests.java
+++ spring-cloud-gcp-pubsub/src/test/java/com/google/cloud/spring/pubsub/support/DefaultSubscriberFactoryTests.java
@@ -769,6 +769,7 @@ class DefaultSubscriberFactoryTests {
     when(this.mockTransportChannel.getEmptyCallContext()).thenReturn(this.mockApiCallContext);
     when(this.mockApiCallContext.withCredentials(any())).thenReturn(this.mockApiCallContext);
     when(this.mockApiCallContext.withTransportChannel(any())).thenReturn(this.mockApiCallContext);
+    when(this.mockApiCallContext.withEndpointContext(any())).thenReturn(this.mockApiCallContext);
 
     GcpProjectIdProvider projectIdProvider = () -> "project";
     DefaultSubscriberFactory factory =
