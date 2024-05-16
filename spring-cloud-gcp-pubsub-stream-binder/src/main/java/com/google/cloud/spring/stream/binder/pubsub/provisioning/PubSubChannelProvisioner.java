63480d1fe5f71db7b1249a8042d09bdf24434936
Jacob Mitash <jacob@mitash.org>
2024-05-16 15:01:28 -0400

    feat: configurable expiration policy on auto-created subscriptions (#2894)
    * Add properties for Binder expiration policy
    
    * Set Expiration Policy when creating Subscription
    
    * Treat non-positive durations as "never expire"
    
    * Use Google Java formatting

--- spring-cloud-gcp-pubsub-stream-binder/src/main/java/com/google/cloud/spring/stream/binder/pubsub/provisioning/PubSubChannelProvisioner.java
+++ spring-cloud-gcp-pubsub-stream-binder/src/main/java/com/google/cloud/spring/stream/binder/pubsub/provisioning/PubSubChannelProvisioner.java
@@ -21,6 +21,7 @@ import com.google.cloud.spring.pubsub.PubSubAdmin;
 import com.google.cloud.spring.stream.binder.pubsub.properties.PubSubConsumerProperties;
 import com.google.cloud.spring.stream.binder.pubsub.properties.PubSubProducerProperties;
 import com.google.pubsub.v1.DeadLetterPolicy;
+import com.google.pubsub.v1.ExpirationPolicy;
 import com.google.pubsub.v1.Subscription;
 import com.google.pubsub.v1.Topic;
 import com.google.pubsub.v1.TopicName;
@@ -71,8 +72,6 @@ public class PubSubChannelProvisioner
     String customName = properties.getExtension().getSubscriptionName();
 
     boolean autoCreate = properties.getExtension().isAutoCreateResources();
-    PubSubConsumerProperties.DeadLetterPolicy deadLetterPolicy =
-        properties.getExtension().getDeadLetterPolicy();
 
     // topicName may be either the short or fully-qualified version.
     String topicShortName =
@@ -101,7 +100,7 @@ public class PubSubChannelProvisioner
         subscriptionName = "anonymous." + topicShortName + "." + UUID.randomUUID();
         this.anonymousGroupSubscriptionNames.add(subscriptionName);
       }
-      ensureSubscriptionExists(subscriptionName, topicName, deadLetterPolicy, autoCreate);
+      ensureSubscriptionExists(subscriptionName, topicName, properties.getExtension(), autoCreate);
     }
 
     Assert.hasText(subscriptionName, "Subscription Name cannot be null or empty");
@@ -142,11 +141,11 @@ public class PubSubChannelProvisioner
   Subscription ensureSubscriptionExists(
       String subscriptionName,
       String topicName,
-      PubSubConsumerProperties.DeadLetterPolicy deadLetterPolicy,
+      PubSubConsumerProperties properties,
       boolean autoCreate) {
     Subscription subscription = this.pubSubAdmin.getSubscription(subscriptionName);
     if (subscription == null) {
-      return createSubscription(subscriptionName, topicName, deadLetterPolicy, autoCreate);
+      return createSubscription(subscriptionName, topicName, properties, autoCreate);
     }
     return subscription;
   }
@@ -154,11 +153,12 @@ public class PubSubChannelProvisioner
   private Subscription createSubscription(
       String subscriptionName,
       String topicName,
-      PubSubConsumerProperties.DeadLetterPolicy deadLetterPolicy,
+      PubSubConsumerProperties properties,
       boolean autoCreate) {
     Subscription.Builder builder =
         Subscription.newBuilder().setName(subscriptionName).setTopic(topicName);
 
+    PubSubConsumerProperties.DeadLetterPolicy deadLetterPolicy = properties.getDeadLetterPolicy();
     if (deadLetterPolicy != null) {
       String dlTopicName = deadLetterPolicy.getDeadLetterTopic();
       Assert.hasText(dlTopicName, "Dead letter policy cannot have null or empty topic");
@@ -173,6 +173,19 @@ public class PubSubChannelProvisioner
         dlpBuilder.setMaxDeliveryAttempts(maxAttempts);
       }
       builder.setDeadLetterPolicy(dlpBuilder);
+    }
+
+    PubSubConsumerProperties.ExpirationPolicy expirationPolicy = properties.getExpirationPolicy();
+    if (expirationPolicy != null) {
+      ExpirationPolicy.Builder epBuilder = ExpirationPolicy.newBuilder();
+
+      if (expirationPolicy.getTtl() != null) {
+        long desiredSeconds = expirationPolicy.getTtl().getSeconds();
+        epBuilder.setTtl(
+            com.google.protobuf.Duration.newBuilder().setSeconds(desiredSeconds).build());
+      }
+
+      builder.setExpirationPolicy(epBuilder);
     }
 
     return this.pubSubAdmin.createSubscription(builder);
