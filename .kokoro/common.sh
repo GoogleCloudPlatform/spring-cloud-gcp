c0cd72809e061d8cbe2d0ba57284792f45f565d5
ldetmer <1771267+ldetmer@users.noreply.github.com>
2024-11-05 11:12:47 -0500

    release debugging

--- .kokoro/common.sh
+++ .kokoro/common.sh
@@ -23,6 +23,8 @@ setup_environment_secrets() {
   export GPG_TTY=$(tty)
   export GPG_HOMEDIR=${TMPDIR}/gpg
   mkdir $GPG_HOMEDIR
+  echo "content of '$KOKORO_KEYSTORE_DIR'"
+  ls -l "${KOKORO_KEYSTORE_DIR}"
   mv ${KOKORO_KEYSTORE_DIR}/70247_maven-gpg-pubkeyring $GPG_HOMEDIR/pubring.gpg
   mv ${KOKORO_KEYSTORE_DIR}/70247_maven-gpg-keyring $GPG_HOMEDIR/secring.gpg
   export SONATYPE_USERNAME=$(cat ${KOKORO_KEYSTORE_DIR}/70247_sonatype-credentials | cut -f1 -d'|')
