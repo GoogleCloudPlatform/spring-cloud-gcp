e01c28443a24e49fbdb77158162bc7b38f126198
ldetmer <1771267+ldetmer@users.noreply.github.com>
2024-11-06 09:29:48 -0500

    trying to get 3.x wokring

--- .kokoro/stage.sh
+++ .kokoro/stage.sh
@@ -24,7 +24,9 @@ source $dir/common.sh
 pushd $dir/../
 
 MAVEN_SETTINGS_FILE=$(realpath .)/settings.xml
-
+echo "test"
+ls "${KOKORO_PIPER_DIR}"
+ls ${KOKORO_PIPER_DIR}
 setup_environment_secrets
 create_settings_xml_file $MAVEN_SETTINGS_FILE
 
