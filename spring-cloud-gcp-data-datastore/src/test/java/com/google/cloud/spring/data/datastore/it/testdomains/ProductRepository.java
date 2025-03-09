fb27879a45672e9ced2d4250dfa9aef5ce0bbfa4
Diego Marquez <diegomarquezp@google.com>
2025-03-09 16:42:06 -0400

    correct whitespace

--- spring-cloud-gcp-data-datastore/src/test/java/com/google/cloud/spring/data/datastore/it/testdomains/ProductRepository.java
+++ spring-cloud-gcp-data-datastore/src/test/java/com/google/cloud/spring/data/datastore/it/testdomains/ProductRepository.java
@@ -22,4 +22,4 @@ import com.google.cloud.spring.data.datastore.repository.DatastoreRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends DatastoreRepository<Product, Key> { }
+public interface ProductRepository extends DatastoreRepository<Product, Key> {}
