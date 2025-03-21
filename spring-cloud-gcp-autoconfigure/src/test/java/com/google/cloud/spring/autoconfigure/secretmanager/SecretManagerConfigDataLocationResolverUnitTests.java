d3dca7213ae9ae02b9f31498a16f64929eae29b1
abheda-crest <alfatah.bheda@crestdata.ai>
2025-03-21 14:08:32 +0530

    secret-manager: fixed test cases

--- spring-cloud-gcp-autoconfigure/src/test/java/com/google/cloud/spring/autoconfigure/secretmanager/SecretManagerConfigDataLocationResolverUnitTests.java
+++ spring-cloud-gcp-autoconfigure/src/test/java/com/google/cloud/spring/autoconfigure/secretmanager/SecretManagerConfigDataLocationResolverUnitTests.java
@@ -12,10 +12,8 @@ import com.google.cloud.spring.core.Credentials;
 import com.google.cloud.spring.core.DefaultGcpProjectIdProvider;
 import com.google.cloud.spring.core.GcpProjectIdProvider;
 import com.google.cloud.spring.secretmanager.SecretManagerServiceClientFactory;
-
 import java.util.List;
 import java.util.stream.Stream;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -102,18 +100,44 @@ class SecretManagerConfigDataLocationResolverUnitTests {
           .isEqualTo(secretManagerServiceClientFactory);
     }
 
+    @Test
+    void testSecretManagerProjectIdTakesPrecedence() {
+      when(secretManagerProperties.getProjectId()).thenReturn("secret-manager-property-id");
+      when(gcpProperties.getProjectId()).thenReturn("gcp-project-id");
+      GcpProjectIdProvider projectIdProvider =
+          SecretManagerConfigDataLocationResolver.createProjectIdProvider(context);
+      assertThat(projectIdProvider.getProjectId()).isEqualTo("secret-manager-property-id");
+    }
+
+    @Test
+    void testProjectIdUseCoreWhenNoSecretManagerProjectId() {
+      when(gcpProperties.getProjectId()).thenReturn("gcp-project-id");
+      GcpProjectIdProvider projectIdProvider =
+          SecretManagerConfigDataLocationResolver.createProjectIdProvider(context);
+      assertThat(projectIdProvider.getProjectId()).isEqualTo("gcp-project-id");
+    }
+
+    @Test
+    void testProjectIdFallBackToDefault() {
+      GcpProjectIdProvider projectIdProvider =
+          SecretManagerConfigDataLocationResolver.createProjectIdProvider(context);
+      assertThat(projectIdProvider).isInstanceOf(DefaultGcpProjectIdProvider.class);
+    }
+
     @BeforeEach
     void registerBean() {
-      GcpSecretManagerProperties properties = mock(GcpSecretManagerProperties.class);
       Credentials credentials = mock(Credentials.class);
       CredentialsProvider credentialsProvider = mock(CredentialsProvider.class);
       SecretManagerServiceClient secretManagerServiceClient =
           mock(SecretManagerServiceClient.class);
 
-      when(properties.getCredentials()).thenReturn(credentials);
+      when(gcpProperties.getCredentials()).thenReturn(credentials);
+      when(secretManagerProperties.getCredentials()).thenReturn(credentials);
 
       defaultBootstrapContext.register(
-          GcpSecretManagerProperties.class, BootstrapRegistry.InstanceSupplier.of(properties));
+          GcpProperties.class, BootstrapRegistry.InstanceSupplier.of(gcpProperties));
+      defaultBootstrapContext.register(
+          GcpSecretManagerProperties.class, BootstrapRegistry.InstanceSupplier.of(secretManagerProperties));
       defaultBootstrapContext.register(
           CredentialsProvider.class, BootstrapRegistry.InstanceSupplier.of(credentialsProvider));
       defaultBootstrapContext.register(
@@ -204,7 +228,6 @@ class SecretManagerConfigDataLocationResolverUnitTests {
 
     @BeforeEach
     void registerBean() {
-      GcpSecretManagerProperties properties = mock(GcpSecretManagerProperties.class);
       Credentials credentials = mock(Credentials.class);
       CredentialsProvider credentialsProvider = mock(CredentialsProvider.class);
       SecretManagerServiceClient secretManagerServiceClient =
@@ -212,19 +235,14 @@ class SecretManagerConfigDataLocationResolverUnitTests {
       SecretManagerServiceClientFactory secretManagerServiceClientFactory =
           mock(SecretManagerServiceClientFactory.class);
 
+      when(gcpProperties.getCredentials()).thenReturn(credentials);
+      when(secretManagerProperties.getCredentials()).thenReturn(credentials);
+
       defaultBootstrapContext.register(
           GcpProperties.class, BootstrapRegistry.InstanceSupplier.of(gcpProperties));
       defaultBootstrapContext.register(
           GcpSecretManagerProperties.class,
           BootstrapRegistry.InstanceSupplier.of(secretManagerProperties));
-      defaultBootstrapContext.register(CredentialsProvider.class,
-          BootstrapRegistry.InstanceSupplier.of(credentialsProvider));
-      defaultBootstrapContext.register(SecretManagerServiceClient.class,
-          BootstrapRegistry.InstanceSupplier.of(secretManagerServiceClient));
-      when(properties.getCredentials()).thenReturn(credentials);
-
-      defaultBootstrapContext.register(
-          GcpSecretManagerProperties.class, BootstrapRegistry.InstanceSupplier.of(properties));
       defaultBootstrapContext.register(
           CredentialsProvider.class, BootstrapRegistry.InstanceSupplier.of(credentialsProvider));
       defaultBootstrapContext.register(
