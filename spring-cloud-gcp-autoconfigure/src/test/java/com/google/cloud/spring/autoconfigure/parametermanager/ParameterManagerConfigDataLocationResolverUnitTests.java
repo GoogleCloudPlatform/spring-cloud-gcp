5b25d2d6043f51114250c6a61cb710af17845ff5
mehdi0933 <mehdibenhalla2012@gmail.com>
2025-08-26 20:11:46 +0530

    feat(parametermanager): add support for gcp parameter manager service (#3699)
    Issue:
    [#3698](https://github.com/GoogleCloudPlatform/spring-cloud-gcp/issues/3698)
    
    **Description**
    This pull request introduces a feature to the Spring Cloud GCP library,
    adding support for the parameter manager module. This enhancement
    enables the library to resolve the parameter presented in GCP Parameter
    Manager in the `application.properties` file and using the `@Value`
    annotation. It also expose the CRUD methods for the Parameter Manager.
    It will be implemented the same way as `secretmanager` is implemented.
    
    **Proposed Changes:**
    - Add the support of `parametermanager` module in the s`pring-cloud-gcp`
    which provides below functionality.
    - Inject parameter from Parameter Manager using `@Value` annotation with
    specified path i.e `pm@pm@global/my-param/v1`
    - Resolve the parameter from the Parameter Manager specified in the
    `application.properties` file
    - Expose the methods for `createParameter`, `enableParameterVersion`,
    `disableParameterVersion`, `deleteParameterVersion`,
    `getParameterString`, `deleteParameter`,
    `renderedParameterVersionString`
    - Add sample application which uses `parametermanager` to showcase the
    usecases
    - Add the documentation to reflect the new capabilities of
    `parametermanager`
    
    Refer to the
    [parametermanager.adoc](https://github.com/GoogleCloudPlatform/spring-cloud-gcp/blob/2aecef8378274397ee667dd1a9fbcf72da43355d/docs/src/main/asciidoc/parametermanager.adoc)
    file for detailed documentation.
    
    Performed the below mentioned manual unit tests to validate the working
    of the global and regional parameter operations.
    * Create parameter with given location, parameterId, version and payload
    * Create parameter with given project, location, parameterId, version
    and payload
    * Create parameter with given location, parameterId, version and
    payload, format
    * Create parameter with given project, location, parameterId, version
    and payload, format
    * Create parameter with given location, parameterId, version and payload
    and invalid projectId (project on which user doesn't have access)
    * Enable parameter version with given location, parameterId and version
    * Enable parameter version with given project, location, parameterId and
    version
    * Disable parameter version with given location, parameterId and version
    * Disable parameter version with given project, location, parameterId
    and version
    * Delete parameter with location and parameterId
    * Delete parameter with project, location and parameterId
    * Delete parameter version with location, parameterId and version
    * Delete parameter version with project, location, parameterId and
    version
    * Check if parameter exist with location and parameterId
    * Check if parameter exist with project, location and parameterId
    * Check if parameter version exist with location, parameterId and
    version
    * Check if parameter version exist with project, location, parameterId
    and version
    * Read parameter with parameter Identifier
    * Render parameter with parameter Identifier
    * Inject with `@value` annotation
    * Read parameter with project, location, parameterId and version for
    other project using service account
    * Check parameter resolution mentioned in the `application.properties`.
    
    More information about parameter manager:
    https://cloud.google.com/secret-manager/parameter-manager/docs/overview

---
+++ spring-cloud-gcp-autoconfigure/src/test/java/com/google/cloud/spring/autoconfigure/parametermanager/ParameterManagerConfigDataLocationResolverUnitTests.java
@@ -0,0 +1,254 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.spring.autoconfigure.parametermanager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.cloud.parametermanager.v1.ParameterManagerClient;
+import com.google.cloud.spring.autoconfigure.core.GcpProperties;
+import com.google.cloud.spring.core.Credentials;
+import com.google.cloud.spring.core.DefaultGcpProjectIdProvider;
+import com.google.cloud.spring.core.GcpProjectIdProvider;
+import com.google.cloud.spring.parametermanager.ParameterManagerClientFactory;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.boot.BootstrapRegistry;
+import org.springframework.boot.DefaultBootstrapContext;
+import org.springframework.boot.context.config.ConfigDataLocation;
+import org.springframework.boot.context.config.ConfigDataLocationResolverContext;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/** Unit tests for {@link ParameterManagerConfigDataLocationResolver} */
+class ParameterManagerConfigDataLocationResolverUnitTests {
+
+  private final ParameterManagerConfigDataLocationResolver resolver =
+      new ParameterManagerConfigDataLocationResolver();
+  private final ConfigDataLocationResolverContext context =
+      mock(ConfigDataLocationResolverContext.class);
+  private final DefaultBootstrapContext defaultBootstrapContext = new DefaultBootstrapContext();
+  private final GcpParameterManagerProperties parameterManagerProperties =
+      mock(GcpParameterManagerProperties.class);
+  private final GcpProperties gcpProperties = mock(GcpProperties.class);
+
+  @Nested
+  class WithStandardBeans {
+    @Test
+    void isResolvableReturnsFalseWithIncorrectPrefix() {
+      assertThat(resolver.isResolvable(context, ConfigDataLocation.of("test://"))).isFalse();
+      assertThat(resolver.isResolvable(context, ConfigDataLocation.of("pm:"))).isFalse();
+    }
+
+    @Test
+    void isResolvableReturnsFalseWithCorrectPrefix() {
+      assertThat(resolver.isResolvable(context, ConfigDataLocation.of("pm@"))).isTrue();
+    }
+
+    @Test
+    void resolveReturnsConfigDataLocation() {
+      List<ParameterManagerConfigDataResource> locations =
+          resolver.resolve(context, ConfigDataLocation.of("pm@my-parameter"));
+      assertThat(locations).hasSize(1);
+      assertThat(locations)
+          .first()
+          .extracting("location")
+          .isEqualTo(ConfigDataLocation.of("pm@my-parameter"));
+      ConfigurableApplicationContext applicationContext =
+          mock(ConfigurableApplicationContext.class);
+      when(applicationContext.getBeanFactory()).thenReturn(new DefaultListableBeanFactory());
+      assertThatCode(() -> defaultBootstrapContext.close(applicationContext))
+          .doesNotThrowAnyException();
+    }
+
+    @Test
+    void createParameterManagerClientWithPresetClientTest() {
+      ParameterManagerClient client = mock(ParameterManagerClient.class);
+      ParameterManagerConfigDataLocationResolver.setParameterManagerClient(client);
+      assertThat(ParameterManagerConfigDataLocationResolver.createParameterManagerClient(context))
+          .isEqualTo(client);
+    }
+
+    @Test
+    void createParameterManagerClientFactoryWithPresetClientTest() {
+      ParameterManagerClientFactory parameterManagerClientFactory =
+          mock(ParameterManagerClientFactory.class);
+      ParameterManagerConfigDataLocationResolver.setParameterManagerClientFactory(
+          parameterManagerClientFactory);
+      assertThat(
+              ParameterManagerConfigDataLocationResolver.createParameterManagerClientFactory(
+                  context))
+          .isEqualTo(parameterManagerClientFactory);
+    }
+
+    @Test
+    void testParameterManagerProjectIdTakesPrecedence() {
+      when(parameterManagerProperties.getProjectId()).thenReturn("parameter-manager-property-id");
+      when(gcpProperties.getProjectId()).thenReturn("gcp-project-id");
+      GcpProjectIdProvider projectIdProvider =
+          ParameterManagerConfigDataLocationResolver.createProjectIdProvider(context);
+      assertThat(projectIdProvider.getProjectId()).isEqualTo("parameter-manager-property-id");
+    }
+
+    @Test
+    void testProjectIdUseCoreWhenNoParameterManagerProjectId() {
+      when(gcpProperties.getProjectId()).thenReturn("gcp-project-id");
+      GcpProjectIdProvider projectIdProvider =
+          ParameterManagerConfigDataLocationResolver.createProjectIdProvider(context);
+      assertThat(projectIdProvider.getProjectId()).isEqualTo("gcp-project-id");
+    }
+
+    @Test
+    void testProjectIdFallBackToDefault() {
+      GcpProjectIdProvider projectIdProvider =
+          ParameterManagerConfigDataLocationResolver.createProjectIdProvider(context);
+      assertThat(projectIdProvider).isInstanceOf(DefaultGcpProjectIdProvider.class);
+    }
+
+    @BeforeEach
+    void registerBean() {
+      Credentials credentials = mock(Credentials.class);
+      CredentialsProvider credentialsProvider = mock(CredentialsProvider.class);
+      ParameterManagerClient parameterManagerServiceClient = mock(ParameterManagerClient.class);
+
+      when(gcpProperties.getCredentials()).thenReturn(credentials);
+      when(parameterManagerProperties.getCredentials()).thenReturn(credentials);
+
+      defaultBootstrapContext.register(
+          GcpProperties.class, BootstrapRegistry.InstanceSupplier.of(gcpProperties));
+      defaultBootstrapContext.register(
+          GcpParameterManagerProperties.class,
+          BootstrapRegistry.InstanceSupplier.of(parameterManagerProperties));
+      defaultBootstrapContext.register(
+          CredentialsProvider.class, BootstrapRegistry.InstanceSupplier.of(credentialsProvider));
+      defaultBootstrapContext.register(
+          ParameterManagerClient.class,
+          BootstrapRegistry.InstanceSupplier.of(parameterManagerServiceClient));
+      when(context.getBinder()).thenReturn(new Binder());
+      when(context.getBootstrapContext()).thenReturn(defaultBootstrapContext);
+    }
+  }
+
+  @Nested
+  class WithFactoryBean {
+    @Test
+    void isResolvableReturnsFalseWithIncorrectPrefix() {
+      assertThat(resolver.isResolvable(context, ConfigDataLocation.of("test://"))).isFalse();
+      assertThat(resolver.isResolvable(context, ConfigDataLocation.of("pm:"))).isFalse();
+    }
+
+    @Test
+    void isResolvableReturnsFalseWithCorrectPrefix() {
+      assertThat(resolver.isResolvable(context, ConfigDataLocation.of("pm@"))).isTrue();
+    }
+
+    @Test
+    void resolveReturnsConfigDataLocation() {
+      List<ParameterManagerConfigDataResource> locations =
+          resolver.resolve(context, ConfigDataLocation.of("pm@my-parameter"));
+      assertThat(locations).hasSize(1);
+      assertThat(locations)
+          .first()
+          .extracting("location")
+          .isEqualTo(ConfigDataLocation.of("pm@my-parameter"));
+      ConfigurableApplicationContext applicationContext =
+          mock(ConfigurableApplicationContext.class);
+      when(applicationContext.getBeanFactory()).thenReturn(new DefaultListableBeanFactory());
+      assertThatCode(() -> defaultBootstrapContext.close(applicationContext))
+          .doesNotThrowAnyException();
+    }
+
+    @Test
+    void createParameterManagerClientWithPresetClientTest() {
+      ParameterManagerClient client = mock(ParameterManagerClient.class);
+      ParameterManagerConfigDataLocationResolver.setParameterManagerClient(client);
+      assertThat(ParameterManagerConfigDataLocationResolver.createParameterManagerClient(context))
+          .isEqualTo(client);
+    }
+
+    @Test
+    void createParameterManagerClientFactoryWithPresetClientTest() {
+      ParameterManagerClientFactory parameterManagerClientFactory =
+          mock(ParameterManagerClientFactory.class);
+      ParameterManagerConfigDataLocationResolver.setParameterManagerClientFactory(
+          parameterManagerClientFactory);
+      assertThat(
+              ParameterManagerConfigDataLocationResolver.createParameterManagerClientFactory(
+                  context))
+          .isEqualTo(parameterManagerClientFactory);
+    }
+
+    @Test
+    void testParameterManagerProjectIdTakesPrecedence() {
+      when(parameterManagerProperties.getProjectId()).thenReturn("parameter-manager-property-id");
+      when(gcpProperties.getProjectId()).thenReturn("gcp-project-id");
+      GcpProjectIdProvider projectIdProvider =
+          ParameterManagerConfigDataLocationResolver.createProjectIdProvider(context);
+      assertThat(projectIdProvider.getProjectId()).isEqualTo("parameter-manager-property-id");
+    }
+
+    @Test
+    void testProjectIdUseCoreWhenNoParameterManagerProjectId() {
+      when(gcpProperties.getProjectId()).thenReturn("gcp-project-id");
+      GcpProjectIdProvider projectIdProvider =
+          ParameterManagerConfigDataLocationResolver.createProjectIdProvider(context);
+      assertThat(projectIdProvider.getProjectId()).isEqualTo("gcp-project-id");
+    }
+
+    @Test
+    void testProjectIdFallBackToDefault() {
+      GcpProjectIdProvider projectIdProvider =
+          ParameterManagerConfigDataLocationResolver.createProjectIdProvider(context);
+      assertThat(projectIdProvider).isInstanceOf(DefaultGcpProjectIdProvider.class);
+    }
+
+    @BeforeEach
+    void registerBean() {
+      Credentials credentials = mock(Credentials.class);
+      CredentialsProvider credentialsProvider = mock(CredentialsProvider.class);
+      ParameterManagerClient parameterManagerServiceClient = mock(ParameterManagerClient.class);
+      ParameterManagerClientFactory parameterManagerClientFactory =
+          mock(ParameterManagerClientFactory.class);
+
+      when(gcpProperties.getCredentials()).thenReturn(credentials);
+      when(parameterManagerProperties.getCredentials()).thenReturn(credentials);
+
+      defaultBootstrapContext.register(
+          GcpProperties.class, BootstrapRegistry.InstanceSupplier.of(gcpProperties));
+      defaultBootstrapContext.register(
+          GcpParameterManagerProperties.class,
+          BootstrapRegistry.InstanceSupplier.of(parameterManagerProperties));
+      defaultBootstrapContext.register(
+          CredentialsProvider.class, BootstrapRegistry.InstanceSupplier.of(credentialsProvider));
+      defaultBootstrapContext.register(
+          ParameterManagerClient.class,
+          BootstrapRegistry.InstanceSupplier.of(parameterManagerServiceClient));
+      defaultBootstrapContext.register(
+          ParameterManagerClientFactory.class,
+          BootstrapRegistry.InstanceSupplier.of(parameterManagerClientFactory));
+
+      when(context.getBinder()).thenReturn(new Binder());
+      when(context.getBootstrapContext()).thenReturn(defaultBootstrapContext);
+    }
+  }
+}
