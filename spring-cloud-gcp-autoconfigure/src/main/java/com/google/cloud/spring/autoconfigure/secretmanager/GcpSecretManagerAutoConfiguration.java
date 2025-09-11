9abfa53828793c16a04551ef3059e9fe4b1b8716
suvidha-malaviya <suvidha.malaviya@crestdata.ai>
2025-09-11 20:08:43 +0530

    fix(parametermanager): fix invalid bean definition named 'clientFactory' (#4127)
    Issue:
    [#4117](https://github.com/GoogleCloudPlatform/spring-cloud-gcp/issues/4117)
    
    Changes:
    
    - Make Parameter Manager as `optional dependency` in AutoConfigure
    module.
    - Changed the `clientFactory` bean name for both Secret Manager and
    Parameter Manager.
    - Added logic to resolve the registered bean in `spring.factories` only
    if the class is present in the classpath for Parameter Manager.
    - Added parameter manager module and starter module in
    `spring-cloud-gcp-dependencies`.
    
    Testing:
    - [x] Created project which uses secretManager
    - [x] Created project which uses parameterManager
    - [x] Created project which uses secretManager and parameterManager

--- spring-cloud-gcp-autoconfigure/src/main/java/com/google/cloud/spring/autoconfigure/secretmanager/GcpSecretManagerAutoConfiguration.java
+++ spring-cloud-gcp-autoconfigure/src/main/java/com/google/cloud/spring/autoconfigure/secretmanager/GcpSecretManagerAutoConfiguration.java
@@ -78,4 +78,10 @@ public class GcpSecretManagerAutoConfiguration {
     return new SecretManagerTemplate(client, this.gcpProjectIdProvider)
         .setAllowDefaultSecretValue(this.properties.isAllowDefaultSecret());
   }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public SecretManagerServiceClientFactory secretManagerClientFactory() {
+    return new DefaultSecretManagerServiceClientFactory(this.credentialsProvider);
+  }
 }
