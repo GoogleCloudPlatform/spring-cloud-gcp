dabcfc89993658798548b4780b98d99055e78b9c
Sakthivel Subramanian <179120858+sakthivelmanii@users.noreply.github.com>
2026-04-20 21:35:01 +0530

    fix(spanner): support querying UUID against STRING columns by using untyped binding (#4416)
    Binds java.util.UUID parameters as Untyped so that BE resolves the
    correct type which solves the issues with legacy schemas using STRING
    columns.
    
    Fixes:
    https://github.com/GoogleCloudPlatform/spring-cloud-gcp/issues/4120

---
+++ spring-cloud-gcp-data-spanner/src/test/java/com/google/cloud/spring/data/spanner/test/domain/UuidUser.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.spring.data.spanner.test.domain;
+
+import com.google.cloud.spring.data.spanner.core.mapping.Column;
+import com.google.cloud.spring.data.spanner.core.mapping.PrimaryKey;
+import com.google.cloud.spring.data.spanner.core.mapping.Table;
+import java.util.UUID;
+
+@Table(name = "uuid_users")
+public class UuidUser {
+
+  @PrimaryKey
+  @Column(name = "user_id")
+  private UUID userId;
+
+  private String name;
+
+  private UUID secondaryId;
+
+  public UuidUser() {}
+
+  public UuidUser(UUID userId, String name) {
+    this.userId = userId;
+    this.name = name;
+  }
+
+  public UUID getUserId() {
+    return this.userId;
+  }
+
+  public void setUserId(UUID userId) {
+    this.userId = userId;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  public UUID getSecondaryId() {
+    return this.secondaryId;
+  }
+
+  public void setSecondaryId(UUID secondaryId) {
+    this.secondaryId = secondaryId;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    UuidUser uuidUser = (UuidUser) o;
+
+    if (userId != null ? !userId.equals(uuidUser.userId) : uuidUser.userId != null) return false;
+    if (name != null ? !name.equals(uuidUser.name) : uuidUser.name != null) return false;
+    return secondaryId != null ? secondaryId.equals(uuidUser.secondaryId) : uuidUser.secondaryId == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = userId != null ? userId.hashCode() : 0;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    result = 31 * result + (secondaryId != null ? secondaryId.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "UuidUser{" + "userId=" + userId + ", name='" + name + '\'' + ", secondaryId=" + secondaryId + '}';
+  }
+}
